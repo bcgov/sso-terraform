c8a8871099d561df6e976c068035d05a49422acd
Junmin Ahn <junmin@button.is>
2021-05-31 10:23:42 -0700

    test: connect gh idp in ci

--- examples/next-app/cypress/integration/actions.spec.js
+++ examples/next-app/cypress/integration/actions.spec.js
@@ -1,14 +1,18 @@
 /// <reference types="cypress" />
 
-describe('Create a new application', () => {
-  it('can submit an application', () => {
-    cy.visit('http://127.0.0.1:3000');
+describe('Test Keycloak client GitHub IDP', () => {
+  const appUrl = Cypress.env('APP_URL') || 'http://127.0.0.1:3000';
+  const ghUsername = Cypress.env('GH_USERNAME') || '';
+  const ghPassword = Cypress.env('GH_PASSWORD') || '';
+
+  it('can login via GitHub IDP', () => {
+    cy.visit(appUrl);
     cy.get('#login').click();
 
-    cy.get('#username').clear().type('testuser');
-    cy.get('#password').clear().type('testuser');
-    cy.get('#kc-login').click();
+    cy.get('#social-github').click();
 
-    // cy.get('#social-github').click();
+    cy.get('#login_field').clear().type(ghUsername);
+    cy.get('#password').clear().type(ghPassword);
+    cy.get('input[type=submit]').click();
   });
 });
