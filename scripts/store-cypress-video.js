ff2d877bad04b4ab8aa2957d17f7ffa15677efea
Junmin Ahn <junmin@button.is>
2021-05-31 12:01:59 -0700

    test: push cypress artifacts in branch

--- scripts/store-cypress-video.js
+++
@@ -1,5 +0,0 @@
-// This module runs in GitHub Action `github-script`
-// see https://github.com/actions/github-script#run-a-separate-file
-module.exports = ({ github, context }) => {
-  return context;
-};
