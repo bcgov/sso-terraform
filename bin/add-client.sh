5cd20f0380deb2fec55cbbddec77fdc83d8da561
Junmin Ahn <junmin@button.is>
2021-06-02 00:45:32 -0700

    ci: support private clients for testing

--- bin/add-client.sh
+++ bin/add-client.sh
@@ -2,7 +2,7 @@
 
 help="\
     Usages:
-        $0 <realm> <client> [--dev] [--test] [--prod] or
+        $0 <realm> <client> [--dev] [--test] [--prod] [--ci] or
 
     Available Realms:
        - onestopauth
@@ -14,6 +14,9 @@ help="\
         --dev   Create a client in Dev environment.
         --test  Create a client in Test environment.
         --prod  Create a client in Prod environment.
+
+    Options:
+        --ci    Set client secret to 'Pa55w0rd' for testing in CI.
     "
 
 if [ "$#" -lt 2 ]; then
@@ -26,10 +29,18 @@ realm="$1"
 client="$2"
 export realm client
 
+# Check if the realm is available
 if [[ ! " ${available_realms[@]} " =~ " ${realm} " ]]; then
     echo -e "$help"
     exit 0
 fi
+
+# Set client secret for testing in CI
+for var in "$@"; do
+    if [ "$var" == "--ci" ]; then
+        export extra_attr='client_secret       = "Pa55w0rd"'
+    fi
+done
 
 pwd="$(dirname "$0")"
 terraform="$pwd/../terraform"
@@ -46,15 +57,15 @@ shift
 while [[ "$1" == -* ]]; do
     case $1 in
     -d | --dev)
-        # echo "$template" >"$dev"
+        echo "$template" >"$dev"
         echo "The client '${client}' generated in '$dev'"
         ;;
     -t | --test)
-        # echo "$template" >"$test"
+        echo "$template" >"$test"
         echo "The client '${client}' generated in '$test'"
         ;;
     -p | --prod)
-        # echo "$template" >"$prod"
+        echo "$template" >"$prod"
         echo "The client '${client}' generated in '$prod'"
         ;;
     -h | --help)
