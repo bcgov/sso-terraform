141c266b8d9b89ff8ee437507e71c708f079b8bd
Junmin Ahn <junmin@button.is>
2021-06-11 13:54:36 -0700

    chore: remove unused bash script

--- bin/terraform-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pwd="$(dirname "$0")"
-basedir="$pwd/../terraform"
-terraform init "$basedir/keycloak-dev/realms/${realm}/client-${client}.tf"
-test="$basedir/keycloak-test/realms/${realm}/client-${client}.tf"
-prod="$basedir/keycloak-prod/realms/${realm}/client-${client}.tf"
