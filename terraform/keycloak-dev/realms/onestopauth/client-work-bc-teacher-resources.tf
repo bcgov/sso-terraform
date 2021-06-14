0fb4de26c95ef51dfa232fb6a6cb485fba87cbf6
Junmin Ahn <junmin@button.is>
2021-06-14 13:59:21 -0700

    chore: update redirect url for client 'work-bc-teacher-resources'

--- terraform/keycloak-dev/realms/onestopauth/client-work-bc-teacher-resources.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-work-bc-teacher-resources.tf
@@ -3,6 +3,6 @@ module "client_work-bc-teacher-resources" {
 
   realm_id            = data.keycloak_realm.this.id
   client_name         = "work-bc-teacher-resources"
-  valid_redirect_uris = ["*"]
+  valid_redirect_uris = ["http://teacher-resources-bdaa18-dev.apps.silver.devops.gov.bc.ca/"]
 
 }
