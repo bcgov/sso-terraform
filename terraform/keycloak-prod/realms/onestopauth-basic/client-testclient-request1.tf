ac0491c9e16fe6605d7071517468d26c594633c3
Junmin Ahn <junmin@button.is>
2021-06-15 13:34:32 -0700

    chore: delete test client in prod environment

--- terraform/keycloak-prod/realms/onestopauth-basic/client-testclient-request1.tf
+++
@@ -1,8 +0,0 @@
-module "client_testclient-request1" {
-  source = "../../../modules/openid-client"
-
-  realm_id            = data.keycloak_realm.this.id
-  client_name         = "testclient-request1"
-  valid_redirect_uris = ["*"]
-
-}
