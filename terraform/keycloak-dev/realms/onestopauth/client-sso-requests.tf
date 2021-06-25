dd07e2b2fa78acc4f9b6986e2c24252851013b97
Junmin Ahn <junmin@button.is>
2021-06-25 11:46:20 -0700

    chore: add web_origins and pkce_code_challenge_method for sso-requests

--- terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
@@ -1,8 +1,10 @@
 module "client_sso-requests" {
   source = "../../../modules/openid-client"
 
-  realm_id            = data.keycloak_realm.this.id
-  client_name         = "sso-requests"
-  access_type         = "PUBLIC"
-  valid_redirect_uris = ["*"]
+  realm_id                   = data.keycloak_realm.this.id
+  client_name                = "sso-requests"
+  access_type                = "PUBLIC"
+  valid_redirect_uris        = ["*"]
+  web_origins                = ["*"]
+  pkce_code_challenge_method = "S256"
 }
