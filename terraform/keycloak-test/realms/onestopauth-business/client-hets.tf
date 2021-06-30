79d498e3bb9a9164919c66e6e844abe17835a48e
nvunnamm <niranchandra.vunnam@cgi.com>
2021-06-30 17:57:45 -0400

    feat: add a new client for 'gets' in dev and test env

---
+++ terraform/keycloak-test/realms/onestopauth-business/client-hets.tf
@@ -0,0 +1,8 @@
+module "client_hets" {
+  source = "../../../modules/openid-client"
+
+  realm_id            = data.keycloak_realm.this.id
+  client_name         = "hets"
+  valid_redirect_uris = ["https://hets-e0cee6-test.apps.silver.devops.gov.bc.ca", "https://hets-e0cee6-uat.apps.silver.devops.gov.bc.ca", "https://hets-e0cee6-train.apps.silver.devops.gov.bc.ca", "https://tst-hets.th.gov.bc.ca", "https://uat-hets.th.gov.bc.ca", "https://trn-hets.th.gov.bc.ca"]
+
+}
