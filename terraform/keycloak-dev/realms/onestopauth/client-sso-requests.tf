cf171c4af179f2e7009d72cc40acdf0c7c0de171
Junmin Ahn <junmin@button.is>
2021-07-07 09:20:37 -0700

    chore: update redirect url for sso-requests

--- terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
@@ -4,7 +4,7 @@ module "client_sso-requests" {
   realm_id                   = data.keycloak_realm.this.id
   client_name                = "sso-requests"
   access_type                = "PUBLIC"
-  valid_redirect_uris        = ["*"]
-  web_origins                = ["*"]
+  valid_redirect_uris        = ["https://bcgov.github.io/sso-terraform-dev"]
+  web_origins                = ["https://bcgov.github.io"]
   pkce_code_challenge_method = "S256"
 }
