bf11f75ca44fa125d443a2cabfc0736b8e369741
Junmin Ahn <junmin@button.is>
2021-07-08 17:01:37 -0700

    chore: add localhost url for redirect urls

--- terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
@@ -4,7 +4,7 @@ module "client_sso-requests" {
   realm_id                   = data.keycloak_realm.this.id
   client_name                = "sso-requests"
   access_type                = "PUBLIC"
-  valid_redirect_uris        = ["https://bcgov.github.io/sso-terraform-dev"]
-  web_origins                = ["https://bcgov.github.io"]
+  valid_redirect_uris        = ["https://bcgov.github.io/sso-terraform-dev", "http://localhost:3000"]
+  web_origins                = ["https://bcgov.github.io", "http://localhost:3000"]
   pkce_code_challenge_method = "S256"
 }
