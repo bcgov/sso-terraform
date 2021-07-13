0f265554ba2a633b8e619897e074fe7e6302e2e4
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-07-13 16:18:09 -0700

    feat: updates to dds-portfolio dev for redirect uris

--- terraform/keycloak-dev/realms/onestopauth-basic/client-dds-portfolio.tf
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-dds-portfolio.tf
@@ -3,6 +3,6 @@ module "client_dds-portfolio" {
 
   realm_id            = data.keycloak_realm.this.id
   client_name         = "dds-portfolio"
-  valid_redirect_uris = ["https://oauth2fider.apps.silver.devops.gov.bc.ca", "https://oauth2fider.apps.silver.devops.gov.bc.ca/settings", "https://oauth2fider.apps.silver.devops.gov.bc.ca/oauth/_mtjccwib6k/callback", "http://localhost:8087/*"]
+  valid_redirect_uris = ["https://oauth2fider.apps.silver.devops.gov.bc.ca", "https://oauth2fider.apps.silver.devops.gov.bc.ca/settings", "https://oauth2fider.apps.silver.devops.gov.bc.ca/oauth/_mtjccwib6k/callback", "http://localhost:8087/*", "https://fcbcfider.apps.silver.devops.gov.bc.ca/oauth/*"]
 
 }
