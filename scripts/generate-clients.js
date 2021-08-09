e22d56ff693012a2b32911a05b4f6c3668eb5256
Junmin Ahn <junminahn@outlook.com>
2021-08-09 12:30:41 -0700

    chore: sync githubaction scripts with dev environment

--- scripts/generate-clients.js
+++ scripts/generate-clients.js
@@ -9,33 +9,45 @@ const mock = new TerraformGenerator();
 
 const keycloakRealm = mock.data('keycloak_realm', 'this', {});
 
-const realms = [
-  { realm: 'onestopauth', idp: _.sortBy(['idir', 'github']) },
-  { realm: 'onestopauth-basic', idp: _.sortBy(['idir', 'github', 'bceid-basic']) },
-  { realm: 'onestopauth-both', idp: _.sortBy(['idir', 'github', 'bceid-basic', 'bceid-business']) },
-  { realm: 'onestopauth-business', idp: _.sortBy(['idir', 'github', 'bceid-business']) },
-];
+const realms = ['onestopauth', 'onestopauth-basic', 'onestopauth-both', 'onestopauth-business'];
+const allEnvironments = ['dev', 'test', 'prod'];
 
-module.exports = ({ projectName, identityProviders, validRedirectUrls, environments }) => {
-  const targetRealm = realms.find((realm) => _.isEqual(realm.idp, _.sortBy(identityProviders)));
+module.exports = ({ clientName, realmName, validRedirectUris, environments, publicAccess }) => {
+  if (!realms.includes(realmName)) return null;
 
-  if (!targetRealm) return [];
+  const getEnvPath = (env) => {
+    const outputDir = path.join(`terraform/keycloak-${env}/realms/${realmName}`);
+    const tfFile = `client-${clientName}.tf`;
+    const target = path.join(outputDir, tfFile);
+
+    return {
+      outputDir,
+      tfFile,
+      target,
+    };
+  };
 
   const SEPARATOR = '\n';
 
   const paths = _.map(environments, (env) => {
-    const outputDir = path.join(`terraform/keycloak-${env}/realms/${targetRealm.realm}`);
-    const tfFile = `client-${projectName}.tf`;
-    const target = path.join(outputDir, tfFile);
+    const { outputDir, target } = getEnvPath(env);
 
     const tfg = new TerraformGenerator();
 
-    tfg.module(`client_${projectName}`, {
+    const data = {
       source: '../../../modules/openid-client',
       realm_id: keycloakRealm.attr('id'),
-      client_name: projectName,
-      valid_redirect_uris: validRedirectUrls[env] || validRedirectUrls,
-    });
+      client_name: clientName,
+      valid_redirect_uris: validRedirectUris[env] || validRedirectUris,
+    };
+
+    if (publicAccess === 'true') {
+      data.access_type = 'PUBLIC';
+      data.pkce_code_challenge_method = 'S256';
+      data.web_origins = ['+'];
+    }
+
+    tfg.module(`client_${clientName}`, data);
 
     const result = tfg.generate();
 
@@ -53,5 +65,5 @@ module.exports = ({ projectName, identityProviders, validRedirectUrls, environme
     return target;
   });
 
-  return { realm: targetRealm.realm, paths };
+  return { paths, allPaths: allEnvironments.map((env) => getEnvPath(env).target) };
 };
--- scripts/github-script.js
+++ scripts/github-script.js
@@ -1,120 +1,156 @@
 const fs = require('fs');
 const _ = require('lodash');
 const generateClients = require('./generate-clients');
+const axios = require('axios');
+const API_URL = 'https://gcp9dahm4c.execute-api.ca-central-1.amazonaws.com/test/actions?status=create';
+const axiosConfig = { headers: { Authorization: process.env.GH_SECRET } };
 
 // This module runs in GitHub Action `github-script`
 // see https://github.com/actions/github-script#run-a-separate-file-with-an-async-function
 module.exports = async ({ github, context }) => {
   const { payload } = context;
   const { inputs, repository } = payload;
-
   const owner = repository.owner.login;
   const repo = repository.name;
 
-  let { projectName, identityProviders, validRedirectUrls, environments } = inputs;
-  projectName = _.kebabCase(projectName);
-  identityProviders = JSON.parse(identityProviders);
-  validRedirectUrls = JSON.parse(validRedirectUrls);
-  environments = JSON.parse(environments);
-
-  const { realm, paths } = generateClients({
-    projectName,
-    identityProviders,
-    validRedirectUrls,
-    environments,
-  });
+  let { requestId, clientName, realmName, validRedirectUris, environments, publicAccess } = inputs;
 
   try {
-    const mainRef = await github.git
-      .getRef({
-        owner,
-        repo,
-        ref: `heads/${repository.default_branch}`,
-      })
-      .then(
-        (res) => res.data,
-        (err) => null
-      );
+    console.log(requestId, clientName, realmName, validRedirectUris, environments, publicAccess);
+
+    validRedirectUris = JSON.parse(validRedirectUris);
+    environments = JSON.parse(environments);
+
+    const info = generateClients({
+      clientName,
+      realmName,
+      validRedirectUris,
+      environments,
+      publicAccess,
+    });
+
+    if (!info) throw Error('failed in client creation');
+
+    const { paths, allPaths } = info;
+
+    const mainRef = await github.git.getRef({ owner, repo, ref: `heads/${repository.default_branch}` }).then(
+      (res) => res.data,
+      (err) => null
+    );
 
     if (!mainRef) {
       console.error('no main branch');
     }
 
-    const prBranchName = `request/${projectName}`;
+    const prBranchName = `request/${clientName}-${new Date().getTime()}`;
 
-    let prRef = await github.git
-      .getRef({
-        owner,
-        repo,
-        ref: `heads/${prBranchName}`,
-      })
-      .then(
-        (res) => res.data,
-        (err) => null
-      );
+    await github.git.createRef({ owner, repo, ref: `refs/heads/${prBranchName}`, sha: mainRef.object.sha });
 
-    if (!prRef) {
-      await github.git.createRef({
-        owner,
-        repo,
-        ref: `refs/heads/${prBranchName}`,
-        sha: mainRef.object.sha,
-      });
+    console.log(allPaths);
+
+    // check the number of existing files to check the mode; create, update and delete
+    const allPathSHAs = await Promise.all(allPaths.map((path) => getSHA({ ref: prBranchName, path })));
+
+    console.log(allPathSHAs);
+
+    const existingCount = _.compact(allPathSHAs).length;
+    console.log(existingCount);
+
+    let mode;
+    if (existingCount === 0) {
+      if (paths.length === 0) {
+        throw Error('no clients to create');
+      }
+
+      mode = 'add';
+    } else {
+      if (paths.length === 0) {
+        mode = 'delete';
+      } else {
+        mode = 'update';
+      }
     }
 
+    console.log('mode', mode);
+
+    // delete the files first before creating ones
+    for (let x = 0; x < allPaths.length; x++) {
+      const path = allPaths[x];
+      await github.repos
+        .deleteFile({
+          owner,
+          repo,
+          sha: await getSHA({ ref: prBranchName, path }),
+          branch: prBranchName,
+          path,
+          message: `chore: remove a client file for ${clientName}`,
+        })
+        .then(
+          (res) => res.data,
+          (err) => null
+        );
+    }
+
+    console.log(paths);
+
+    // create the requested client files
     for (let x = 0; x < paths.length; x++) {
+      const path = paths[x];
       await github.repos.createOrUpdateFileContents({
         owner,
         repo,
-        sha: await getSHA({
-          ref: prBranchName,
-          path: paths[x],
-        }),
+        sha: await getSHA({ ref: prBranchName, path }),
         branch: prBranchName,
-        path: paths[x],
-        message: `feat: add a client file for ${projectName}`,
-        content: fs.readFileSync(paths[x], { encoding: 'base64' }),
+        path,
+        message: `feat: add a client file for ${clientName}`,
+        content: fs.readFileSync(path, { encoding: 'base64' }),
       });
     }
 
-    await github.pulls
-      .create({
-        owner,
-        repo,
-        base: repository.default_branch,
-        head: prBranchName,
-        title: `request: add client files for ${projectName}`,
-        body: `
-#### Project Name: \`${_.startCase(projectName)}\`
-#### Identity Providers: \`${identityProviders.join(', ')}\`
-#### Target Realm: \`${realm}\`
-#### Environments: \`${environments.join(', ')}\`
-${environments.map(
-  (env) => `<details><summary>Show Details for ${env}</summary>
-\`\`\`<ul>✔️Valid Redirect Urls${(validRedirectUrls[env] || validRedirectUrls || []).map(
+    let pr = await github.pulls.create({
+      owner,
+      repo,
+      base: repository.default_branch,
+      head: prBranchName,
+      title: `request: ${mode} client files for ${clientName}`,
+      body: `
+  #### Project Name: \`${_.startCase(clientName)}\`
+  #### Target Realm: \`${realmName}\`
+  #### Environments: \`${environments.join(', ')}\`
+  ${environments.map(
+    (env) => `<details><summary>Show Details for ${env}</summary>
+  \`\`\`<ul>✔️Valid Redirect Urls${(validRedirectUris[env] || validRedirectUris || []).map(
     (url) => `<li>${url}</li>`
   )}</ul>\`\`\`
-</details>`
-)}`,
-        maintainer_can_modify: false,
-      })
-      .catch(() => null);
-  } catch (e) {
-    console.log(e);
+  </details>`
+  )}`,
+      maintainer_can_modify: false,
+    });
+
+    const {
+      data: { number },
+    } = pr;
+
+    await github.issues.addLabels({
+      owner,
+      repo,
+      issue_number: number,
+      labels: ['auto_generated']
+    })
+
+    axios.put(API_URL, { prNumber: number, prSuccess: true, id: requestId, actionNumber: context.runId }, axiosConfig);
+    return pr;
+  } catch (err) {
+    console.log(err);
+    axios.put(API_URL, { prNumber: null, prSuccess: false, id: requestId, actionNumber: context.runId }, axiosConfig);
+    throw err;
   }
 
   async function getSHA({ ref, path }) {
-    const data = await github.repos
-      .getContent({
-        owner,
-        repo,
-        ref,
-        path,
-      })
-      .then(
-        (res) => res.data,
-        (err) => null
-      );
+    const data = await github.repos.getContent({ owner, repo, ref, path }).then(
+      (res) => res.data,
+      (err) => null
+    );
 
     return data && data.sha;
   }
