aa0e707bc609f3f01eb812adecd74d45ef081a4e
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-20 11:05:36 -0700

    chore: remove a client file for csb-secure-file-transfer-232

--- terraform/keycloak-prod/realms/onestopauth/client-csb-secure-file-transfer-232.tf
+++
@@ -1,8 +0,0 @@
-module "client_csb-secure-file-transfer-232" {
-  source      = "../../../modules/openid-client"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "csb-secure-file-transfer-232"
-  valid_redirect_uris = [
-    "https://jag.gov.bc.ca/ocj-sft"
-  ]
-}
