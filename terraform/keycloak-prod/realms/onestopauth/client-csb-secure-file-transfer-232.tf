a1383e3336c6ab6568c886eb67750895a2eaf407
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-20 10:23:55 -0700

    feat: add a client file for csb-secure-file-transfer-232

---
+++ terraform/keycloak-prod/realms/onestopauth/client-csb-secure-file-transfer-232.tf
@@ -0,0 +1,8 @@
+module "client_csb-secure-file-transfer-232" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "csb-secure-file-transfer-232"
+  valid_redirect_uris = [
+    "https://jag.gov.bc.ca/csb-sft"
+  ]
+}
