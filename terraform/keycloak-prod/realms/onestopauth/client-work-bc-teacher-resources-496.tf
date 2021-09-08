df3f1b1f5838c3d5bfba0d1bbcec6ed1135e3f37
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-08 14:12:34 -0700

    feat: add a client file for work-bc-teacher-resources-496

---
+++ terraform/keycloak-prod/realms/onestopauth/client-work-bc-teacher-resources-496.tf
@@ -0,0 +1,8 @@
+module "client_work-bc-teacher-resources-496" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "work-bc-teacher-resources-496"
+  valid_redirect_uris = [
+    "https://admin-jobboard.workbc.ca/*"
+  ]
+}
