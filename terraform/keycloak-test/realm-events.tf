079082d300e8a1271fb31497b6bd2601757a8ff7
Junmin Ahn <junminahn@outlook.com>
2021-09-09 20:30:01 -0700

    chore: enable event logs for five most active realms

--- terraform/keycloak-test/realm-events.tf
+++ terraform/keycloak-test/realm-events.tf
@@ -1,6 +1,6 @@
 module "realm_events" {
   source   = "../modules/realm-events"
-  for_each = { for v in var.dev_event_realms : v => v }
+  for_each = { for v in var.test_event_realms : v => v }
 
   realm_id = each.value
 }
