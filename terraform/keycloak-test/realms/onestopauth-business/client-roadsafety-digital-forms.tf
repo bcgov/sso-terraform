46e1c4c521016e2590fa2d1fb2c8d367b84e0263
Jonathan Langlois <jon@button.is>
2021-09-14 15:04:00 -0700

    chore: fmt
    format terraform files

--- terraform/keycloak-test/realms/onestopauth-business/client-roadsafety-digital-forms.tf
+++ terraform/keycloak-test/realms/onestopauth-business/client-roadsafety-digital-forms.tf
@@ -4,6 +4,6 @@ module "client_roadsafety-digital-forms" {
   realm_id            = data.keycloak_realm.this.id
   client_name         = "roadsafety-digital-forms"
   valid_redirect_uris = ["https://rsbc-dh-prohibition-web-app-test.apps.silver.devops.gov.bc.ca/redirect"]
-  web_origins                = ["+"]
-  access_type                = "PUBLIC"
+  web_origins         = ["+"]
+  access_type         = "PUBLIC"
 }
