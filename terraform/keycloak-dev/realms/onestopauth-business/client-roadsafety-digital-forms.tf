1497fcb8918306e56ecac01e2cefc89035872be0
Jonathan Langlois <jon@button.is>
2021-09-14 11:47:21 -0700

    feat: add client
    add client for road safety to onestopauth-business

---
+++ terraform/keycloak-dev/realms/onestopauth-business/client-roadsafety-digital-forms.tf
@@ -0,0 +1,8 @@
+module "client_roadsafety-digital-forms" {
+  source = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+
+  realm_id            = data.keycloak_realm.this.id
+  client_name         = "roadsafety-digital-forms"
+  valid_redirect_uris = ["https://rsbc-dh-prohibition-web-app-dev.apps.silver.devops.gov.bc.ca/redirect"]
+
+}
