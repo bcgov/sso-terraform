6cd4a3ce27a30969d30e4014cfd1cea620fa64d4
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-13 17:03:39 -0700

    chore: remove a client file for csb-secure-file-transfer-232

--- terraform/keycloak-test/realms/onestopauth/client-csb-secure-file-transfer-232.tf
+++
@@ -1,8 +0,0 @@
-module "client_csb-secure-file-transfer-232" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "csb-secure-file-transfer-232"
-  valid_redirect_uris = [
-    "https://test.jag.gov.bc.ca/csb-sft/"
-  ]
-}
