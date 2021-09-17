a9b68541c0e17d2c6ec92574965e283ab4c6ef2d
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-09-17 09:57:06 -0700

    feat: updates to road safety redirect uris

--- terraform/keycloak-dev/realms/onestopauth-business/client-roadsafety-digital-forms.tf
+++ terraform/keycloak-dev/realms/onestopauth-business/client-roadsafety-digital-forms.tf
@@ -3,7 +3,7 @@ module "client_roadsafety-digital-forms" {
 
   realm_id            = data.keycloak_realm.this.id
   client_name         = "roadsafety-digital-forms"
-  valid_redirect_uris = ["https://rsbc-dh-prohibition-web-app-dev.apps.silver.devops.gov.bc.ca/redirect"]
+  valid_redirect_uris = ["https://rsbc-dh-prohibition-web-app-dev.apps.silver.devops.gov.bc.ca/redirect", "http://localhost:8080"]
   web_origins         = ["+"]
   access_type         = "PUBLIC"
 }
