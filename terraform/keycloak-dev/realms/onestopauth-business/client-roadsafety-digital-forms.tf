4f3daaba8f0146610738ead75254eb8bdcf8123c
Jonathan Langlois <jon@button.is>
2021-09-20 09:44:56 -0700

    chore: lifespan
    change client access token lifespan to 8 hours

--- terraform/keycloak-dev/realms/onestopauth-business/client-roadsafety-digital-forms.tf
+++ terraform/keycloak-dev/realms/onestopauth-business/client-roadsafety-digital-forms.tf
@@ -1,9 +1,10 @@
 module "client_roadsafety-digital-forms" {
   source = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
 
-  realm_id            = data.keycloak_realm.this.id
-  client_name         = "roadsafety-digital-forms"
-  valid_redirect_uris = ["https://rsbc-dh-prohibition-web-app-dev.apps.silver.devops.gov.bc.ca/redirect"]
-  web_origins         = ["+"]
-  access_type         = "PUBLIC"
+  realm_id              = data.keycloak_realm.this.id
+  client_name           = "roadsafety-digital-forms"
+  valid_redirect_uris   = ["https://rsbc-dh-prohibition-web-app-dev.apps.silver.devops.gov.bc.ca/redirect"]
+  web_origins           = ["+"]
+  access_type           = "PUBLIC"
+  access_token_lifespan = "28800"
 }
