7d7b6ed2ceb5553fe64c7a7680bcef0b6647b210
Jonathan Langlois <jon@button.is>
2021-09-28 14:56:48 -0700

    chore: add prod
    add production hets client

---
+++ terraform/keycloak-prod/realms/onestopauth-business/client.hets.tf
@@ -0,0 +1,14 @@
+module "client_hets" {
+  source = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "hets"
+  valid_redirect_uris = [
+    "https://hets-e0cee6-prod.apps.silver.devops.gov.bc.ca/*",
+    "https://hets.th.gov.bc.ca/*",
+    "http://localhost:3000/*"
+  ]
+  web_origins                = ["+"]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+}
