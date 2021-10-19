cf40bf6c9de66ab2206c1ccfea2586ea417edb44
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-10-19 09:00:57 -0700

    chore: remove a client file for sso-examples-confidential-563

--- terraform/keycloak-dev/realms/onestopauth/client-sso-examples-confidential-563.tf
+++
@@ -1,9 +0,0 @@
-module "client_sso-examples-confidential-563" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "sso-examples-confidential-563"
-  valid_redirect_uris = [
-    "http://localhost:3000",
-    "http://localhost:3000/*"
-  ]
-}
