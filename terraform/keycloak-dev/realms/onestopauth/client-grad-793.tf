e551281c3748a1556110bdd1a3fbf76f956cf6dc
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-01 11:42:14 -0700

    feat: add a client file for grad-793

---
+++ terraform/keycloak-dev/realms/onestopauth/client-grad-793.tf
@@ -0,0 +1,14 @@
+module "client_grad-793" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "grad-793"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
