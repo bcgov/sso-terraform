08f2e185d92027233f4973036798f01f89bfaee6
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-03 11:28:53 -0700

    chore: remove a client file for some-other-project-894

--- terraform/keycloak-dev/realms/onestopauth-basic/client-some-other-project-894.tf
+++
@@ -1,9 +0,0 @@
-module "client_some-other-project-894" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "some-other-project-894"
-  valid_redirect_uris = [
-    "https://someotherproject.com"
-  ]
-  description = "CSS App Created"
-}
