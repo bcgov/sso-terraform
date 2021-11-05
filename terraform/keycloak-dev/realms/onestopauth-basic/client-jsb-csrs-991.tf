23971503d2d62b613215fe41f73161863d958a25
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-05 13:20:33 -0700

    chore: remove a client file for jsb-csrs-991

--- terraform/keycloak-dev/realms/onestopauth-basic/client-jsb-csrs-991.tf
+++
@@ -1,9 +0,0 @@
-module "client_jsb-csrs-991" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "jsb-csrs-991"
-  valid_redirect_uris = [
-    "https://localhost:8080"
-  ]
-  description = "CSS App Created"
-}
