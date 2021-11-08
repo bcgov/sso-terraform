b9b796fd0cddda2cce39a8ea08f3f93efb91e35c
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-08 09:27:58 -0800

    feat: add a client file for tti-poc-public-1189

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-tti-poc-public-1189.tf
@@ -0,0 +1,14 @@
+module "client_tti-poc-public-1189" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "tti-poc-public-1189"
+  valid_redirect_uris = [
+    "https://civilresolutionbc--springdev.my.salesforce.com/services/authcallback/PathfinderSSOPublic"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
