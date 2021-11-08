08df2ea660edc2f12d3a3b0a3e627193a6ca28f5
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-08 12:20:36 -0800

    feat: add a client file for front-counter-bc-fider-661

---
+++ terraform/keycloak-prod/realms/onestopauth/client-front-counter-bc-fider-661.tf
@@ -0,0 +1,9 @@
+module "client_front-counter-bc-fider-661" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "front-counter-bc-fider-661"
+  valid_redirect_uris = [
+    "https://fcbcfider.apps.silver.devops.gov.bc.ca/"
+  ]
+  description = "CSS App Created"
+}
