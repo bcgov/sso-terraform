ce5a65a1af9d2be69accb77d7ac83313658993ba
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-08 10:18:43 -0800

    chore: remove a client file for realm-profile-app-760

--- terraform/keycloak-test/realms/onestopauth/client-realm-profile-app-760.tf
+++
@@ -1,9 +0,0 @@
-module "client_realm-profile-app-760" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "realm-profile-app-760"
-  valid_redirect_uris = [
-    "http://localhost:3000/*"
-  ]
-  description = "CSS App Created"
-}
