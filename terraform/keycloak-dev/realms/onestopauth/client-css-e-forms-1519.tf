57ec935994d0349ea057727ae418c25d241de9ed
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-23 10:13:14 -0800

    feat: add a client file for css-e-forms-1519

---
+++ terraform/keycloak-dev/realms/onestopauth/client-css-e-forms-1519.tf
@@ -0,0 +1,14 @@
+module "client_css-e-forms-1519" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "css-e-forms-1519"
+  valid_redirect_uris = [
+    "https://web-dev-7d3aa1-dev.apps.silver.devops.gov.bc.ca"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
