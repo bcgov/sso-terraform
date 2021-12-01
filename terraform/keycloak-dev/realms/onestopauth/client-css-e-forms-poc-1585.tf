c4ac61fa974f30a5ba0b538905e4314246784e3b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-01 14:50:41 -0800

    chore: remove a client file for css-e-forms-poc-1585

--- terraform/keycloak-dev/realms/onestopauth/client-css-e-forms-poc-1585.tf
+++
@@ -1,9 +0,0 @@
-module "client_css-e-forms-poc-1585" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "css-e-forms-poc-1585"
-  valid_redirect_uris = [
-    "https://keycloak-7d3aa1-dev.apps.silver.devops.gov.bc.ca/auth/realms/forms-flow-ai/broker/keycloak-oidc/endpoint"
-  ]
-  description = "CSS App Created"
-}
