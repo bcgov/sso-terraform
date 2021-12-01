7fe3b8372642a049fe96226c15dff6e0723b7010
Jonathan Langlois <jon@button.is>
2021-11-30 20:06:58 -0800

    fix: override
    add flag to override auth flow

--- terraform/keycloak-prod/realms/onestopauth/client-work-bc-teacher-resources.tf
+++ terraform/keycloak-prod/realms/onestopauth/client-work-bc-teacher-resources.tf
@@ -6,9 +6,10 @@ data "keycloak_authentication_flow" "work_bc_browserflow" {
 module "client_work-bc-teacher-resources" {
   source = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
 
-  realm_id                    = data.keycloak_realm.this.id
-  client_name                 = "work-bc-teacher-resources"
-  valid_redirect_uris         = ["https://careereducation.workbc.ca/openid-connect/keycloak"]
-  browser_authentication_flow = data.keycloak_authentication_flow.work_bc_browserflow.id
+  realm_id                     = data.keycloak_realm.this.id
+  client_name                  = "work-bc-teacher-resources"
+  valid_redirect_uris          = ["https://careereducation.workbc.ca/openid-connect/keycloak"]
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.work_bc_browserflow.id
 
 }
