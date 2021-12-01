474a1b7cb35882ee144d3264b6c4d116691b2042
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-01 12:33:05 -0800

    chore: remove a client file for front-counter-bc-fider-661

--- terraform/keycloak-dev/realms/onestopauth/client-front-counter-bc-fider-661.tf
+++
@@ -1,9 +0,0 @@
-module "client_front-counter-bc-fider-661" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "front-counter-bc-fider-661"
-  valid_redirect_uris = [
-    "https://v191fider.apps.silver.devops.gov.bc.ca/oauth/_hs72sbrfrh/callback"
-  ]
-  description = "CSS App Created"
-}
