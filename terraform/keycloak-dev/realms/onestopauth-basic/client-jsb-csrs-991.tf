6e422aefb1ce615867750ee97d6a5a3b2ed82f06
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-07 09:31:13 -0800

    feat: add a client file for jsb-csrs-991

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-jsb-csrs-991.tf
@@ -0,0 +1,14 @@
+module "client_jsb-csrs-991" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "jsb-csrs-991"
+  valid_redirect_uris = [
+    "https://jag-csrs-portal-public-f0b5b6-dev.apps.silver.devops.gov.bc.ca"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
