d3b40d437999606a76d4901500427e354a650b6b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-16 15:25:17 -0800

    chore: remove a client file for tti-ams-1486

--- terraform/keycloak-prod/realms/onestopauth/client-tti-ams-1486.tf
+++
@@ -1,9 +0,0 @@
-module "client_tti-ams-1486" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "tti-ams-1486"
-  valid_redirect_uris = [
-    "http://localhost:1000"
-  ]
-  description = "CSS App Created"
-}
