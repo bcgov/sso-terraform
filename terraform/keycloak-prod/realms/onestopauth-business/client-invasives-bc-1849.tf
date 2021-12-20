f30d43fd89561e70b879fdb5a2a4a9c003861b04
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-20 15:52:27 -0800

    chore: remove a client file for invasives-bc-1849

--- terraform/keycloak-prod/realms/onestopauth-business/client-invasives-bc-1849.tf
+++
@@ -1,9 +0,0 @@
-module "client_invasives-bc-1849" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "invasives-bc-1849"
-  valid_redirect_uris = [
-    "https://invasivesbci.apps.silver.devops.gov.bc.ca"
-  ]
-  description = "CSS App Created"
-}
