8cae9badf752bfb5cda352cb697504e6db88c79f
Jonathan Langlois <jon@button.is>
2021-12-21 12:21:55 -0800

    chore: URIs
    update URIs to allow redurection to different pages

--- terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
@@ -4,7 +4,7 @@ module "client_sso-requests" {
   realm_id                   = data.keycloak_realm.this.id
   client_name                = "sso-requests"
   access_type                = "PUBLIC"
-  valid_redirect_uris        = ["https://bcgov.github.io/sso-terraform-dev", "http://localhost:3000"]
+  valid_redirect_uris        = ["https://bcgov.github.io/sso-terraform-dev", "http://localhost:3000", "https://bcgov.github.io/sso-terraform-dev/*", "http://localhost:3000/*"]
   web_origins                = ["https://bcgov.github.io", "http://localhost:3000"]
   pkce_code_challenge_method = "S256"
   roles                      = ["sso-admin", "bceid-approver"]
