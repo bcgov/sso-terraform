e65a36dd850b162d689f1553abfac213f589271e
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-01-05 15:40:06 -0800

    feat: add a client file for realm-profile-app-760

---
+++ terraform/keycloak-test/realms/onestopauth/client-realm-profile-app-760.tf
@@ -0,0 +1,10 @@
+module "client_realm-profile-app-760" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "realm-profile-app-760"
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "http://localhost:8080/*"
+  ]
+  description = "CSS App Created"
+}
