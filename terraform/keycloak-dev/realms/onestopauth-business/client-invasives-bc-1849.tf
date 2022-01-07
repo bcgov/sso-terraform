e62e51de975b3f5141a33871a9af4a56aa035677
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-01-07 09:15:05 -0800

    feat: add a client file for invasives-bc-1849

---
+++ terraform/keycloak-dev/realms/onestopauth-business/client-invasives-bc-1849.tf
@@ -0,0 +1,29 @@
+module "client_invasives-bc-1849" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "invasives-bc-1849"
+  valid_redirect_uris = [
+    "capacitor://127.0.0.1:3000/*",
+    "capacitor://192.168.1.105:8000/*",
+    "capacitor://192.168.1.105:8100/*",
+    "capacitor://localhost:3000/*",
+    "capacitor://localhost:8100/*",
+    "http://127.0.0.1:3000/home/*",
+    "http://192.168.1.105:8000/*",
+    "http://192.168.1.105:8100/*",
+    "http://localhost/*",
+    "http://localhost:3000/*",
+    "https://dev-invasivesbci.apps.silver.devops.gov.bc.ca/home/landing",
+    "invasivesbc://127.0.0.1:3000/*",
+    "invasivesbc://192.168.1.105:8000/*",
+    "invasivesbc://192.168.1.105:8100/*",
+    "invasivesbc://localhost/*",
+    "invasivesbc://localhost:3000/*"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
