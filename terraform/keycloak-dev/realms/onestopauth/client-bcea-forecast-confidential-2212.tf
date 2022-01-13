5d1dd4d994e81125911de009403c1e2a1e5c9c73
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-01-13 13:05:33 -0800

    chore: remove a client file for bcea-forecast-confidential-2212

--- terraform/keycloak-dev/realms/onestopauth/client-bcea-forecast-confidential-2212.tf
+++
@@ -1,9 +0,0 @@
-module "client_bcea-forecast-confidential-2212" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "bcea-forecast-confidential-2212"
-  valid_redirect_uris = [
-    "http://localhost:3000/*"
-  ]
-  description = "CSS App Created"
-}
