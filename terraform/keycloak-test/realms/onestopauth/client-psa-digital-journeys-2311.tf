a88081661a8a110f9102f32a6142e1fa8bc8233a
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-02 08:47:31 -0800

    feat: add a client file for psa-digital-journeys-2311

---
+++ terraform/keycloak-test/realms/onestopauth/client-psa-digital-journeys-2311.tf
@@ -0,0 +1,9 @@
+module "client_psa-digital-journeys-2311" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "psa-digital-journeys-2311"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description = "CSS App Created"
+}
