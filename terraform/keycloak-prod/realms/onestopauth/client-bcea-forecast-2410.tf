d8ea4b2df49c020bee9f2474b3992835fbdf5cc8
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-07 10:24:26 -0800

    chore: remove a client file for bcea-forecast-2410

--- terraform/keycloak-prod/realms/onestopauth/client-bcea-forecast-2410.tf
+++
@@ -1,9 +0,0 @@
-module "client_bcea-forecast-2410" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "bcea-forecast-2410"
-  valid_redirect_uris = [
-    "http://localhost:3000/"
-  ]
-  description = "CSS App Created"
-}
