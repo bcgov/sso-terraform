3192916a7f75c3c6de31b400cf52d491a4260101
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-07 13:26:31 -0800

    chore: remove a client file for psa-moodle-2773

--- terraform/keycloak-dev/realms/onestopauth-basic/client-psa-moodle-2773.tf
+++
@@ -1,9 +0,0 @@
-module "client_psa-moodle-2773" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "psa-moodle-2773"
-  valid_redirect_uris = [
-    "https://moodle-950003-dev.apps.silver.devops.gov.bc.ca/admin/oauth2callback.php"
-  ]
-  description = "CSS App Created"
-}
