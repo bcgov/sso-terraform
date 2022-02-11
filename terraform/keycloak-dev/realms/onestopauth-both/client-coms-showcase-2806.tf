ae7549b321e88c23e739866740761f07a3378859
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-11 11:39:09 -0800

    chore: remove a client file for coms-showcase-2806

--- terraform/keycloak-dev/realms/onestopauth-both/client-coms-showcase-2806.tf
+++
@@ -1,9 +0,0 @@
-module "client_coms-showcase-2806" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "coms-showcase-2806"
-  valid_redirect_uris = [
-    "http://localhost:3000"
-  ]
-  description = "CSS App Created"
-}
