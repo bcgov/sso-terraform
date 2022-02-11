d99c4bfd37d0b6b8849bf61c21e3067556cad4e5
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-11 09:29:08 -0800

    feat: add a client file for coms-showcase-frontend-localhost-2872

---
+++ terraform/keycloak-dev/realms/onestopauth-both/client-coms-showcase-frontend-localhost-2872.tf
@@ -0,0 +1,19 @@
+module "client_coms-showcase-frontend-localhost-2872" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "coms-showcase-frontend-localhost-2872"
+  valid_redirect_uris = [
+    "http://localhost:3000",
+    "http://localhost:8080/*",
+    "http://localhost:8081/*"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "http://localhost:8080/*",
+    "http://localhost:8081/*",
+    "+"
+  ]
+}
