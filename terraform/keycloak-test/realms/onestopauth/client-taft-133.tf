3d0de90109ef877c90f04004c438e44ec499e30b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-25 11:05:51 -0800

    feat: add a client file for taft-133

---
+++ terraform/keycloak-test/realms/onestopauth/client-taft-133.tf
@@ -0,0 +1,12 @@
+module "client_taft-133" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "taft-133"
+  valid_redirect_uris = [
+    "https://idm-demo.taft.fin.gov.bc.ca/auth/realms/taft/broker/idir/endpoint/*",
+    "https://idm-test-taft.apps.silver.devops.gov.bc.ca/auth/realms/taft/broker/idir/endpoint/*",
+    "https://idm-test.taft.fin.gov.bc.ca/auth/realms/taft/broker/idir/endpoint/*",
+    "https://localhost:8080/auth/realms/taft/broker/idir/endpoint/*"
+  ]
+  description = "CSS App Created"
+}
