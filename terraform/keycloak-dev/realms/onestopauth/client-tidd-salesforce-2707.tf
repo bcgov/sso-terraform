bb27ebbc368d22e30617563f1165f3490e2cb49a
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-09 09:28:36 -0800

    chore: remove a client file for tidd-salesforce-2707

--- terraform/keycloak-dev/realms/onestopauth/client-tidd-salesforce-2707.tf
+++
@@ -1,9 +0,0 @@
-module "client_tidd-salesforce-2707" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "tidd-salesforce-2707"
-  valid_redirect_uris = [
-    "https://tradeandinvestbc--meldev.my.salesforce.com/"
-  ]
-  description = "CSS App Created"
-}
