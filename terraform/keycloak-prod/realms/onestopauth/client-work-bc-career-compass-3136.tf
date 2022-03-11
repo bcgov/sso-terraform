77d415081837a6cbb7fcbf85c53407d5225bd4d8
Junmin Ahn <36021827+junminahn@users.noreply.github.com>
2022-03-10 17:12:44 -0800

    feat: add a client file for work-bc-career-compass-3136

---
+++ terraform/keycloak-prod/realms/onestopauth/client-work-bc-career-compass-3136.tf
@@ -0,0 +1,15 @@
+module "client_work-bc-career-compass-3136" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "work-bc-career-compass-3136"
+  valid_redirect_uris = [
+    "https://careerdiscoveryquizzes/openid-connect/keycloak"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://careerdiscoveryquizzes/openid-connect/keycloak",
+    "+"
+  ]
+}
