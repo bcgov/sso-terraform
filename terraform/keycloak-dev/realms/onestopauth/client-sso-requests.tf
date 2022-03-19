ecb90119275637854395c135e4b614c08d38ee68
Junmin Ahn <junminahn@outlook.com>
2022-03-18 22:49:04 -0700

    chore: add new site urls into valid redirect uris

--- terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-sso-requests.tf
@@ -1,10 +1,15 @@
 module "client_sso-requests" {
   source = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
 
-  realm_id                   = data.keycloak_realm.this.id
-  client_name                = "sso-requests"
-  access_type                = "PUBLIC"
-  valid_redirect_uris        = ["https://bcgov.github.io/sso-terraform-dev", "http://localhost:3000", "https://bcgov.github.io/sso-terraform-dev/*", "http://localhost:3000/*"]
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "sso-requests"
+  access_type = "PUBLIC"
+  valid_redirect_uris = [
+    "https://bcgov.github.io/sso-terraform-dev/*",
+    "https://bcgov.github.io/sso-requests-dev/*",
+    "https://bcgov.github.io/sso-requests-test/*",
+    "http://localhost:3000/*"
+  ]
   web_origins                = ["https://bcgov.github.io", "http://localhost:3000"]
   pkce_code_challenge_method = "S256"
   roles                      = ["sso-admin", "bceid-approver"]
