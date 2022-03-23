23a5760789a478ed2a0367f2f4dfe782d68f6766
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-23 12:21:12 -0700

    chore: remove a client file for coco-experiment-3368

--- terraform/keycloak-dev/realms/onestopauth/client-coco-experiment-3368.tf
+++
@@ -1,9 +0,0 @@
-module "client_coco-experiment-3368" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "coco-experiment-3368"
-  valid_redirect_uris = [
-    "http://192.168.1.65:8082/app/"
-  ]
-  description = "CSS App Created"
-}
