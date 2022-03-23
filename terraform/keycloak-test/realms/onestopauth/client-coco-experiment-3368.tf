b3f3afa94670b1cf142c28544788b2a9e96cd902
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-23 12:21:15 -0700

    feat: add a client file for coco-experiment-3368

---
+++ terraform/keycloak-test/realms/onestopauth/client-coco-experiment-3368.tf
@@ -0,0 +1,10 @@
+module "client_coco-experiment-3368" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "coco-experiment-3368"
+  valid_redirect_uris = [
+    "http://localhost:8081/*",
+    "http://localhost:8082/*"
+  ]
+  description = "CSS App Created"
+}
