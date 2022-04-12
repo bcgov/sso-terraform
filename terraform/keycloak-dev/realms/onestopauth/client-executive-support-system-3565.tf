d41df114bb6be99ce3b79ae0fab6fe46db17e60a
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-12 11:59:49 -0700

    chore: remove a client file for executive-support-system-3565

--- terraform/keycloak-dev/realms/onestopauth/client-executive-support-system-3565.tf
+++
@@ -1,9 +0,0 @@
-module "client_executive-support-system-3565" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "executive-support-system-3565"
-  valid_redirect_uris = [
-    "https://dev.cabops.gov.bc.ca/"
-  ]
-  description = "CSS App Created"
-}
