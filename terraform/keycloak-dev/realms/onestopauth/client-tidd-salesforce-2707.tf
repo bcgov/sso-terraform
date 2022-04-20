e8e7a39681e29b2afbffa08e4b0c3052a3a9b6f6
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-20 13:01:25 -0700

    feat: add a client file for tidd-salesforce-2707

---
+++ terraform/keycloak-dev/realms/onestopauth/client-tidd-salesforce-2707.tf
@@ -0,0 +1,10 @@
+module "client_tidd-salesforce-2707" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "tidd-salesforce-2707"
+  valid_redirect_uris = [
+    "https://tradeandinvestbc--meldev.my.salesforce.com/services/authcallback/PathfinderIdir",
+    "https://tradeandinvestbclightning--dev.my.salesforce.com/services/authcallback/PathfinderIdir"
+  ]
+  description = "CSS App Created"
+}
