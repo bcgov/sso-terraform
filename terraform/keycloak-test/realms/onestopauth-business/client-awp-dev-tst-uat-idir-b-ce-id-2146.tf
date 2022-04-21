f7b37fc864244e8ee5d0b8b64363027fca7a101f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-21 10:43:49 -0700

    feat: add a client file for awp-dev-tst-uat-idir-b-ce-id-2146

---
+++ terraform/keycloak-test/realms/onestopauth-business/client-awp-dev-tst-uat-idir-b-ce-id-2146.tf
@@ -0,0 +1,11 @@
+module "client_awp-dev-tst-uat-idir-b-ce-id-2146" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "awp-dev-tst-uat-idir-b-ce-id-2146"
+  valid_redirect_uris = [
+    "https://tst-awp-cloak-proxied.th.gov.bc.ca/*",
+    "https://tst-awp.th.gov.bc.ca/*",
+    "https://uat-awp.th.gov.bc.ca/*"
+  ]
+  description = "CSS App Created"
+}
