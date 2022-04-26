7a00849b9396e39bf8de9b633a0862a9ec5b7f2f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-26 11:07:23 -0700

    chore: remove a client file for ti-gr-s-3467

--- terraform/keycloak-test/realms/onestopauth/client-ti-gr-s-3467.tf
+++
@@ -1,9 +0,0 @@
-module "client_ti-gr-s-3467" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "ti-gr-s-3467"
-  valid_redirect_uris = [
-    "https://bcmot--test.my.salesforce.com/services/authcallback/PathfinderIdir"
-  ]
-  description = "CSS App Created"
-}
