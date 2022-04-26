e793fb52272d407713b711d819d6df82d6d60db8
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-26 11:07:24 -0700

    feat: add a client file for ti-gr-s-3467

---
+++ terraform/keycloak-dev/realms/onestopauth/client-ti-gr-s-3467.tf
@@ -0,0 +1,9 @@
+module "client_ti-gr-s-3467" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "ti-gr-s-3467"
+  valid_redirect_uris = [
+    "https://bcmot--dev2.my.salesforce.com/services/authcallback/BCGovSSO"
+  ]
+  description = "CSS App Created"
+}
