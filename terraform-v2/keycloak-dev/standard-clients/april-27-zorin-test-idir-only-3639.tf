90595d356e61db07946ccdfa4fa2fa40eccfc3e7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-27 10:55:52 -0700

    feat: add a client file for april-27-zorin-test-idir-only-3639

---
+++ terraform-v2/keycloak-dev/standard-clients/april-27-zorin-test-idir-only-3639.tf
@@ -0,0 +1,24 @@
+module "april-27-zorin-test-idir-only-3639" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_name = "april-27-zorin-test-idir-only-3639"
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+}
