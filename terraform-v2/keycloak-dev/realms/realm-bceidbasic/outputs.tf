9060b086beeb6a44fe5be1f1dd3f70922a0ca3d1
Junmin Ahn <junminahn@outlook.com>
2022-04-27 10:44:34 -0700

    chore: add preview site for valid redirect uris

--- terraform-v2/keycloak-dev/realms/realm-bceidbasic/outputs.tf
+++ terraform-v2/keycloak-dev/realms/realm-bceidbasic/outputs.tf
@@ -6,3 +6,11 @@ output "standard_client_secret" {
   value     = module.standard_client.client_secret
   sensitive = true
 }
+
+output "realm_id" {
+  value = module.realm.id
+}
+
+output "realm_name" {
+  value = module.realm.name
+}
