462ee9aeca3ae63c0565925e4eafbe56d51ca5ce
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-29 11:49:57 -0700

    chore: remove a client file for awp-prd-idir-only-3634

--- terraform/keycloak-prod/realms/onestopauth/client-awp-prd-idir-only-3634.tf
+++
@@ -1,9 +0,0 @@
-module "client_awp-prd-idir-only-3634" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "awp-prd-idir-only-3634"
-  valid_redirect_uris = [
-    "https://awp.th.gov.bc.ca/*"
-  ]
-  description = "CSS App Created"
-}
