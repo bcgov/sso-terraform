5b393049183708ae99ed04aa5a1484d75d003199
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-03 08:20:00 -0700

    chore: remove a client file for psa-digital-journeys-2311

--- terraform/keycloak-prod/realms/onestopauth/client-psa-digital-journeys-2311.tf
+++
@@ -1,9 +0,0 @@
-module "client_psa-digital-journeys-2311" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "psa-digital-journeys-2311"
-  valid_redirect_uris = [
-    "https://digital-journeys-keycloak.apps.silver.devops.gov.bc.ca/auth/realms/psa-digital-journey/*"
-  ]
-  description = "CSS App Created"
-}
