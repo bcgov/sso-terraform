a8b679f473680ca905cb60e2f9d9bace72dfcaa7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-03 13:00:33 -0700

    chore: remove a client file for sdpr-research-portal-3669

--- terraform/keycloak-test/realms/onestopauth/client-sdpr-research-portal-3669.tf
+++
@@ -1,9 +0,0 @@
-module "client_sdpr-research-portal-3669" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "sdpr-research-portal-3669"
-  valid_redirect_uris = [
-    "https://research-portal-ba7244-test.apps.silver.devops.gov.bc.ca/"
-  ]
-  description = "CSS App Created"
-}
