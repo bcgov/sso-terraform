44fe6fa0dc43278b5911d9f7d8d4112f452bf35e
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-05 12:38:34 -0700

    chore: remove a client file for met-3668

--- terraform/keycloak-dev/realms/onestopauth/client-met-3668.tf
+++
@@ -1,9 +0,0 @@
-module "client_met-3668" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "met-3668"
-  valid_redirect_uris = [
-    "https://keycloak-e903c2-dev.apps.gold.devops.gov.bc.ca/auth/realms/met/broker/IDIR/endpoint"
-  ]
-  description = "CSS App Created"
-}
