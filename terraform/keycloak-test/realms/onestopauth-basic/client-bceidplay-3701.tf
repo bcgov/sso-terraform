5ca616c4499f8e8fddd5af78e8d8fdc1b855f431
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-20 12:12:38 -0700

    feat: add a client file for bceidplay-3701

---
+++ terraform/keycloak-test/realms/onestopauth-basic/client-bceidplay-3701.tf
@@ -0,0 +1,9 @@
+module "client_bceidplay-3701" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "bceidplay-3701"
+  valid_redirect_uris = [
+    "https://bceid-test.solnspace.com"
+  ]
+  description = "CSS App Created"
+}
