e46f5ccde212096251c8f67f438787d70beda1f3
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-24 15:35:47 -0700

    chore: remove a client file for fam-3702

--- terraform/keycloak-dev/realms/onestopauth/client-fam-3702.tf
+++
@@ -1,9 +0,0 @@
-module "client_fam-3702" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "fam-3702"
-  valid_redirect_uris = [
-    "https://oidcdebuggersecure-3d5c3f-dev.apps.silver.devops.gov.bc.ca/*"
-  ]
-  description = "CSS App Created"
-}
