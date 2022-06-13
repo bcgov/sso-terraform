eca3b76c3cea35a5e8a74d9e3114b14e923e4de1
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-13 09:01:05 -0700

    feat: add a client file for sdpr-research-portal-3669

---
+++ terraform/keycloak-dev/realms/onestopauth/client-sdpr-research-portal-3669.tf
@@ -0,0 +1,11 @@
+module "client_sdpr-research-portal-3669" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "sdpr-research-portal-3669"
+  client_name = "sdpr-research-portal-3669"
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://research-portal-ba7244-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+  description = "CSS App Created"
+}
