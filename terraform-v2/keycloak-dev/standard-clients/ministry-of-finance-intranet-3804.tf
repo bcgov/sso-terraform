d0fd87561ea783a85807c778382c4c94934460be
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-17 12:05:39 -0700

    feat: add a client file for ministry-of-finance-intranet-3804

---
+++ terraform-v2/keycloak-dev/standard-clients/ministry-of-finance-intranet-3804.tf
@@ -0,0 +1,28 @@
+data "keycloak_authentication_flow" "ministry_of_finance_intranet_3804_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "ministry-of-finance-intranet-3804" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "ministry-of-finance-intranet-3804"
+  client_name = "Ministry of Finance Intranet"
+  valid_redirect_uris = [
+    "https://dev.intranet.fin.gov.bc.ca/",
+    "https://dev.intranet.fin.gov.bc.ca/*",
+    "https://dev.intranet.fin.gov.bc.ca/openid-connect/keycloak"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.ministry_of_finance_intranet_3804_browserflow.id
+}
