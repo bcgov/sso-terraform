8b64cb16f233fb7d99c39ef5185aee0cf6dd38df
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-17 15:36:53 -0700

    feat: add a client file for bcea-forecast-2410

---
+++ terraform/keycloak-dev/realms/onestopauth/client-bcea-forecast-2410.tf
@@ -0,0 +1,11 @@
+module "client_bcea-forecast-2410" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "bcea-forecast-2410"
+  client_name = "bcea-forecast-2410"
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://bcea-forecast-d9436d-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+  description = "CSS App Created"
+}
