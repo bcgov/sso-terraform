0f87577705f842dcfc3b3655ef6257877237ff06
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-23 16:02:54 -0700

    feat: add a client file for gdx-agreements-tracker-3833

---
+++ terraform-v2/keycloak-test/standard-clients/gdx-agreements-tracker-3833.tf
@@ -0,0 +1,25 @@
+data "keycloak_authentication_flow" "gdx_agreements_tracker_3833_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "gdx-agreements-tracker-3833" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "gdx-agreements-tracker-3833"
+  client_name = "GDX-Agreements-tracker"
+  valid_redirect_uris = [
+    "https://gdx-agreements-tracker-dev.apps.silver.devops.gov.bc.ca/login?redirect=/"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.gdx_agreements_tracker_3833_browserflow.id
+}
