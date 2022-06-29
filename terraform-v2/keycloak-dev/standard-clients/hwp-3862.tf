db066cc38e99fd3ed6aa8921982df65c2d950465
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-29 10:49:35 -0700

    feat: add a client file for hwp-3862

---
+++ terraform-v2/keycloak-dev/standard-clients/hwp-3862.tf
@@ -0,0 +1,25 @@
+data "keycloak_authentication_flow" "hwp_3862_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "hwp-3862" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "hwp-3862"
+  client_name = "hwp-login"
+  valid_redirect_uris = [
+    "http://localhost:5000/*"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.hwp_3862_browserflow.id
+}
