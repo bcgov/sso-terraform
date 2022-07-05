1e41ced44253a72f853d4209fb4a52c1e74d5553
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-05 16:15:33 -0700

    feat: add a client file for dss-dps-kirk-3928

---
+++ terraform-v2/keycloak-prod/standard-clients/dss-dps-kirk-3928.tf
@@ -0,0 +1,25 @@
+data "keycloak_authentication_flow" "dss_dps_kirk_3928_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "dss-dps-kirk-3928" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "dss-dps-kirk-3928"
+  client_name = "dss-dps-kirk"
+  valid_redirect_uris = [
+    "https://dps-ops-portal.apps.gov.bc.ca/*"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.dss_dps_kirk_3928_browserflow.id
+}
