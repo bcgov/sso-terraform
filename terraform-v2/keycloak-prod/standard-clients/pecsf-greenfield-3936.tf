86c99b8f72733a0017aec922359bc407673547f3
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-14 22:27:23 -0700

    feat: add a client file for pecsf-greenfield-3936

---
+++ terraform-v2/keycloak-prod/standard-clients/pecsf-greenfield-3936.tf
@@ -0,0 +1,26 @@
+data "keycloak_authentication_flow" "pecsf_greenfield_3936_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "pecsf-greenfield-3936" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "pecsf-greenfield-3936"
+  client_name = "PECSF Greenfield"
+  valid_redirect_uris = [
+    "https://pecsf.gov.bc.ca/login/keycloak/callback"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.pecsf_greenfield_3936_browserflow.id
+}
