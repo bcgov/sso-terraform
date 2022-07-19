e3fd8c22741ec2289342f544924b24cfdedb6571
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-19 12:07:46 -0700

    chore: remove a client file for dss-kirk-1288

--- terraform/keycloak-dev/realms/onestopauth/client-dss-kirk-1288.tf
+++
@@ -1,9 +0,0 @@
-module "client_dss-kirk-1288" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "dss-kirk-1288"
-  valid_redirect_uris = [
-    "https://kirk-d.data.gov.bc.ca/"
-  ]
-  description = "CSS App Created"
-}
