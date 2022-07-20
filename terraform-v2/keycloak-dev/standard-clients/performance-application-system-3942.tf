390c41a5c4cac817919b784cb642a7f26106cd89
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-19 17:41:06 -0700

    feat: add a client file for performance-application-system-3942

---
+++ terraform-v2/keycloak-dev/standard-clients/performance-application-system-3942.tf
@@ -0,0 +1,27 @@
+data "keycloak_authentication_flow" "performance_application_system_3942_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "performance-application-system-3942" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "performance-application-system-3942"
+  client_name = "Performance ASystem"
+  valid_redirect_uris = [
+    "http://localhost:8000/login/keycloak/callback",
+    "https://performance-332842-dev.apps.silver.devops.gov.bc.ca/login/keycloak/callback"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.performance_application_system_3942_browserflow.id
+}
