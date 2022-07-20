f7b90d60f2176e77330c7711909c82afa91c9964
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-20 07:43:56 -0700

    chore: remove a client file for slam-3637

--- terraform/keycloak-prod/realms/onestopauth/client-slam-3637.tf
+++
@@ -1,9 +0,0 @@
-module "client_slam-3637" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "slam-3637"
-  valid_redirect_uris = [
-    "https://citz-imb-slam-ca94a8.apps.silver.devops.gov.bc.ca/"
-  ]
-  description = "CSS App Created"
-}
