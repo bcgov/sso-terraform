be140cd67e1494c9028ffbbbd5c6e95cc3a6fb82
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-04 15:45:07 -0700

    feat: add a client file for aqss-3932

---
+++ terraform-v2/keycloak-dev/standard-clients/aqss-3932.tf
@@ -0,0 +1,35 @@
+data "keycloak_authentication_flow" "aqss_3932_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "aqss-3932" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "aqss-3932"
+  client_name                         = "AQSS"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.aqss_3932_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "https://aqadvisories-eservice-ef3999-dev.apps.silver.devops.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000",
+    "https://aqadvisories-eservice-ef3999-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
