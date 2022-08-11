25e7c8c142e9bf1f2a1f45feb3fee040877a35ba
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-11 11:06:18 -0700

    feat: add a client file for trans-action-3979

---
+++ terraform-v2/keycloak-prod/standard-clients/trans-action-3979.tf
@@ -0,0 +1,27 @@
+data "keycloak_authentication_flow" "trans_action_3979_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "trans-action-3979" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "trans-action-3979"
+  client_name                         = "TransAction"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.trans_action_3979_browserflow.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://transaction.th.gov.bc.ca/"
+  ]
+}
