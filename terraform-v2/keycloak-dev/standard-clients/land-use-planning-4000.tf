2132082683f96c6fc940c0e7872ffb6820dc79e3
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-22 15:33:26 -0700

    feat: add a client file for land-use-planning-4000

---
+++ terraform-v2/keycloak-dev/standard-clients/land-use-planning-4000.tf
@@ -0,0 +1,29 @@
+data "keycloak_authentication_flow" "land_use_planning_4000_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "land-use-planning-4000" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "land-use-planning-4000"
+  client_name                         = "Land Use Planning"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.land_use_planning_4000_browserflow.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://localhost:4200/*",
+    "http://localhost:4200/admin/*",
+    "https://localhost:3000/*"
+  ]
+}
