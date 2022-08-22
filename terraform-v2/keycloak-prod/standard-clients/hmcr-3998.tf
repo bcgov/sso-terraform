8be78a742200be793abd8b87ccddd8c485e475be
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-22 15:53:31 -0700

    feat: add a client file for hmcr-3998

---
+++ terraform-v2/keycloak-prod/standard-clients/hmcr-3998.tf
@@ -0,0 +1,27 @@
+data "keycloak_authentication_flow" "hmcr_3998_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "hmcr-3998" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "hmcr-3998"
+  client_name                         = "HMCR"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.hmcr_3998_browserflow.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://hmcr.th.gov.bc.ca/*"
+  ]
+}
