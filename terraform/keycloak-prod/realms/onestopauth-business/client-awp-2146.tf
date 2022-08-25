e5ce6e4ccb7e7e1a67a838f1dfa36ce801ef667d
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-25 11:35:13 -0700

    chore: remove a client file for awp-2146

--- terraform/keycloak-prod/realms/onestopauth-business/client-awp-2146.tf
+++
@@ -1,9 +0,0 @@
-module "client_awp-2146" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "awp-2146"
-  valid_redirect_uris = [
-    "https://awp.th.gov.bc.ca/*"
-  ]
-  description = "CSS App Created"
-}
