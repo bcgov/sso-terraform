f9eb9594bdc64325b47893b5e1c6da9f03ff5be7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-09-07 17:51:13 -0700

    feat: add a client file for fsa-cognito-idir-prod-4089

---
+++ terraform-v2/keycloak-dev/standard-clients/fsa-cognito-idir-prod-4089.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "fsa_cognito_idir_prod_4089_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "fsa-cognito-idir-prod-4089" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "fsa-cognito-idir-prod-4089"
+  client_name                         = "FSA"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.fsa_cognito_idir_prod_4089_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:8080",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:8080"
+  ]
+}
