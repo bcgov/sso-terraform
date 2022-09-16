53c0e2d095fd0538c02c2d686438321adc7efae5
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-16 14:09:42 -0700

    feat: add a client file for awp-3798

---
+++ terraform-v2/keycloak-prod/standard-clients/awp-3798.tf
@@ -0,0 +1,28 @@
+data "keycloak_authentication_flow" "awp_3798_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "awp-3798" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "awp-3798"
+  client_name                         = "SAWSx"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.awp_3798_browserflow.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://awp.th.gov.bc.ca/*"
+  ]
+}
