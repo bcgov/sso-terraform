32137e8906e7281d22b85da8a0c3f83f774f011a
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-23 15:47:38 -0700

    feat: add a client file for fsa-cognito-b-ce-id-business-dev-4090

---
+++ terraform-v2/keycloak-dev/standard-clients/fsa-cognito-b-ce-id-business-dev-4090.tf
@@ -0,0 +1,35 @@
+data "keycloak_authentication_flow" "fsa_cognito_b_ce_id_business_dev_4090_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "fsa-cognito-b-ce-id-business-dev-4090" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "fsa-cognito-b-ce-id-business-dev-4090"
+  client_name                         = "FSA Cognito BCeID Business DEV"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.fsa_cognito_b_ce_id_business_dev_4090_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://fam-cognito-usable-kodiak.auth.ca-central-1.amazoncognito.com/oauth2/idpresponse",
+    "https://nr-custom-auth-test.auth.ca-central-1.amazoncognito.com/oauth2/idpresponse",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://fam-cognito-usable-kodiak.auth.ca-central-1.amazoncognito.com/oauth2/idpresponse",
+    "https://nr-custom-auth-test.auth.ca-central-1.amazoncognito.com/oauth2/idpresponse"
+  ]
+}
