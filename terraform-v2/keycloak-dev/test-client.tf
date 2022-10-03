3c0d62f0a726ea62d05d8911d8be460cf6384250
Junmin Ahn <junminahn@outlook.com>
2022-10-03 11:56:15 -0700

    chore: add test client in gold dev for testing

---
+++ terraform-v2/keycloak-dev/test-client.tf
@@ -0,0 +1,45 @@
+resource "keycloak_openid_client" "test_client" {
+  realm_id = module.standard.realm_id
+
+  client_id = "test-client"
+  name      = "SSO Dev Test"
+
+  enabled                      = true
+  standard_flow_enabled        = true
+  implicit_flow_enabled        = false
+  direct_access_grants_enabled = false
+  service_accounts_enabled     = false
+
+  access_type   = "PUBLIC"
+  client_secret = ""
+
+  valid_redirect_uris = ["https://bcgov.github.io/keycloak-example-apps/*"]
+  web_origins         = ["+"]
+
+  authentication_flow_binding_overrides {
+    browser_id = module.idp_auth_flow.flow_id
+  }
+}
+
+data "keycloak_authentication_flow" "idp_stopper" {
+  realm_id = module.standard.realm_id
+  alias    = "idp stopper"
+}
+
+
+resource "keycloak_openid_client_default_scopes" "test_client_default_scopes" {
+  realm_id  = module.standard.realm_id
+  client_id = data.keycloak_authentication_flow.idp_stopper.id
+
+  default_scopes = [
+    "profile",
+    "email",
+    "common",
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "bceidbusiness",
+    "bceidboth",
+    "github",
+  ]
+}
