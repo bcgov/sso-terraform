304899fca8b8fd878809537e5f624e88ea04869a
ZHE CHEN <kzsyixi@gmail.com>
2022-10-07 09:52:36 -0700

    chore: create custom realm

--- terraform-v2-custom/keycloak-dev/custom-realms/kuro-new-realm.tf
+++ terraform-v2-custom/keycloak-dev/custom-realms/kuro-new-realm.tf
@@ -1,5 +1,5 @@
 module "kuro-new-realm" {
   source     = "github.com/bcgov/sso-terraform-modules?ref=main/modules/custom-realm"
-  realm_name = "kuro-dev-new-realm"
+  realm_name = "kuro-dev-test-new-realm"
   enabled    = true
 }
