a7018bcc0773e694ccb495d89eb48401a688b8d6
tzhang200 <tzhang200@gmail.com>
2022-10-11 13:10:11 -0700

    chore: trailing space

--- terraform-v2-custom/keycloak-dev/main.tf
+++ terraform-v2-custom/keycloak-dev/main.tf
@@ -6,7 +6,7 @@ module "master_viewer_role" {
   source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/master-viewer-role"
   role_name   = "viewer-custom-realms"
   realm_names = [
-    "bcparks-service-transformation", 
+    "bcparks-service-transformation",
     "zorin-dev-custom-realm"
   ]
 
