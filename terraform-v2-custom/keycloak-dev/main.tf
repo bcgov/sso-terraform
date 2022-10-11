e0f0c0432649c7d6c0f114a9ffa735489cb032e1
tzhang200 <tzhang200@gmail.com>
2022-10-11 13:00:15 -0700

    chore: add test custom realm zorin-dev-custom-realm

--- terraform-v2-custom/keycloak-dev/main.tf
+++ terraform-v2-custom/keycloak-dev/main.tf
@@ -5,7 +5,7 @@ module "custom_realms" {
 module "master_viewer_role" {
   source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/master-viewer-role"
   role_name   = "viewer-custom-realms"
-  realm_names = ["bcparks-service-transformation"]
+  realm_names = ["bcparks-service-transformation", "zorin-dev-custom-realm"]
 
   depends_on = [
     module.custom_realms,
