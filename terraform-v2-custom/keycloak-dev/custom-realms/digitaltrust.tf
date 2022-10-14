a540036e3f447fdeb123c918691168e8b0d41698
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-10-14 11:30:07 -0700

    chore: updated digitaltrust realm name

--- terraform-v2-custom/keycloak-dev/custom-realms/digitaltrust.tf
+++ terraform-v2-custom/keycloak-dev/custom-realms/digitaltrust.tf
@@ -1,5 +1,5 @@
-module "digital-trust" {
+module "digitaltrust" {
   source     = "github.com/bcgov/sso-terraform-modules?ref=main/modules/custom-realm"
-  realm_name = "digital-trust"
+  realm_name = "digitaltrust"
   enabled    = true
 }
