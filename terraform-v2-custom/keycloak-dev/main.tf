0d99c75e435618bcd59a580675182cae9c5fb12d
Junmin Ahn <junminahn@outlook.com>
2022-10-14 10:54:15 -0700

    chore: remove master viewer role resource

--- terraform-v2-custom/keycloak-dev/main.tf
+++ terraform-v2-custom/keycloak-dev/main.tf
@@ -1,13 +1,3 @@
 module "custom_realms" {
   source = "./custom-realms"
 }
-
-module "master_viewer_role" {
-  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/master-viewer-role"
-  role_name   = "viewer-custom-realms"
-  realm_names = ["bcparks-service-transformation"]
-
-  depends_on = [
-    module.custom_realms,
-  ]
-}
