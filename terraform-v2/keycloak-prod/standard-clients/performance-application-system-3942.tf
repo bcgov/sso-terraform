86e5eac7465df9167d53925c7fd5213a4e32fb4f
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-14 21:27:18 -0700

    feat: add a client file for performance-application-system-3942

---
+++ terraform-v2/keycloak-prod/standard-clients/performance-application-system-3942.tf
@@ -0,0 +1,24 @@
+module "performance-application-system-3942" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "performance-application-system-3942"
+  client_name                         = "Performance Development Platform"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://performance.gov.bc.ca/login/keycloak/callback"
+  ]
+}
