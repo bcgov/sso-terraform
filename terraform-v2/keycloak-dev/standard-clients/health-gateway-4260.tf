fe3dac58d5bc313bc0dcf093b88e330095901357
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-17 14:25:52 -0700

    feat: add a client file for health-gateway-4260

---
+++ terraform-v2/keycloak-dev/standard-clients/health-gateway-4260.tf
@@ -0,0 +1,23 @@
+module "health-gateway-4260" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "health-gateway-4260"
+  client_name                         = "Health Gateway"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://dev.loginproxy.gov.bc.ca/auth/realms/health-gateway-gold/broker/idir/endpoint"
+  ]
+}
