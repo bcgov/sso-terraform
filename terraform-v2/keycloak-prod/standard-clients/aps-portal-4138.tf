4522d8fd7d9afd77f8bc0b39c5d07f0b44ed55ee
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-17 14:25:17 -0700

    feat: add a client file for aps-portal-4138

---
+++ terraform-v2/keycloak-prod/standard-clients/aps-portal-4138.tf
@@ -0,0 +1,26 @@
+module "aps-portal-4138" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "aps-portal-4138"
+  client_name                         = "APS Portal DEV"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://authz.apps.gov.bc.ca/auth/realms/aps/broker/bceid-business/endpoint",
+    "https://authz.apps.gov.bc.ca/auth/realms/aps/broker/github/endpoint",
+    "https://authz.apps.gov.bc.ca/auth/realms/aps/broker/idir/endpoint"
+  ]
+}
