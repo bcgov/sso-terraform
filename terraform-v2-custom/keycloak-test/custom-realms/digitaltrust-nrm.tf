80147cb51a6d8027d294aa9a4bc1fea4329e7dd0
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-10-18 13:25:47 -0700

    feat: added digitaltrust-nrm terraform

--- terraform-v2-custom/keycloak-test/custom-realms/digitaltrust-nrm.tf
+++ terraform-v2-custom/keycloak-test/custom-realms/digitaltrust-nrm.tf
@@ -1,5 +1,5 @@
-module "digitaltrust" {
+module "digitaltrust-nrm" {
   source     = "github.com/bcgov/sso-terraform-modules?ref=main/modules/custom-realm"
-  realm_name = "digitaltrust"
+  realm_name = "digitaltrust-nrm"
   enabled    = true
 }
