cd14052434b41dc656bb68e9661e5f322025207d
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-19 09:00:01 -0700

    feat: add a client file for work-bc-es-4268

---
+++ terraform-v2/keycloak-dev/standard-clients/work-bc-es-4268.tf
@@ -0,0 +1,25 @@
+module "work-bc-es-4268" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "work-bc-es-4268"
+  client_name                         = "WorkBC ES"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://dev.loginproxy.gov.bc.ca/auth/realms/workbces/*"
+  ]
+}
