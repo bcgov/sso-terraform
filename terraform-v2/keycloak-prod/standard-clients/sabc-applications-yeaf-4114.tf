50f06511cbb174207a46c7dd628613a806b1b0b2
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-19 13:49:41 -0700

    feat: add a client file for sabc-applications-yeaf-4114

---
+++ terraform-v2/keycloak-prod/standard-clients/sabc-applications-yeaf-4114.tf
@@ -0,0 +1,25 @@
+module "sabc-applications-yeaf-4114" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "sabc-applications-yeaf-4114"
+  client_name                         = "SABC Applications - YEAF"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://10.0.0.51:8017/app-login",
+    "https://tbtb-yeaf-06cb9b-prod.apps.silver.devops.gov.bc.ca/app-login"
+  ]
+}
