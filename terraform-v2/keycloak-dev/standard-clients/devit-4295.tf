10787df90a5b183f455f3e8c6911fc02bad45c8e
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-21 08:17:13 -0700

    feat: add a client file for devit-4295

---
+++ terraform-v2/keycloak-dev/standard-clients/devit-4295.tf
@@ -0,0 +1,23 @@
+module "devit-4295" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "devit-4295"
+  client_name                         = "DEVIT"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://devit.gaming.gov.bc.ca/openid-connect/keycloak"
+  ]
+}
