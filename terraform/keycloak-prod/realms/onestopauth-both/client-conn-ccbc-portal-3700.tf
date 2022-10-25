43cf277bfa5f81e174615ad3f106c5ccb85f3606
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-25 15:13:33 -0700

    chore: remove a client file for conn-ccbc-portal-3700

--- terraform/keycloak-prod/realms/onestopauth-both/client-conn-ccbc-portal-3700.tf
+++
@@ -1,9 +0,0 @@
-module "client_conn-ccbc-portal-3700" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "conn-ccbc-portal-3700"
-  valid_redirect_uris = [
-    "https://ccbc.apps.silver.devops.gov.bc.ca/auth-callback"
-  ]
-  description = "CSS App Created"
-}
