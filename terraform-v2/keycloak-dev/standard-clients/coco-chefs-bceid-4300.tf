411889faed79981749e2a6d11745b6bc6787ef8b
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-25 12:05:30 -0700

    feat: add a client file for coco-chefs-bceid-4300

---
+++ terraform-v2/keycloak-dev/standard-clients/coco-chefs-bceid-4300.tf
@@ -0,0 +1,27 @@
+module "coco-chefs-bceid-4300" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "coco-chefs-bceid-4300"
+  client_name                         = "coco-chefs-bceid"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:8080/*",
+    "http://localhost:8081/*",
+    "http://localhost:8082/*"
+  ]
+}
