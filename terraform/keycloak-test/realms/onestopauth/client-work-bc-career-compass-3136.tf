6075ffd5aef4c7e2e63a550dc95b5f3efd2458c8
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-28 10:07:34 -0700

    chore: remove a client file for work-bc-career-compass-3136

--- terraform/keycloak-test/realms/onestopauth/client-work-bc-career-compass-3136.tf
+++
@@ -1,9 +0,0 @@
-module "client_work-bc-career-compass-3136" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "work-bc-career-compass-3136"
-  valid_redirect_uris = [
-    "https://careerdiscoveryquizzes-test.workbc.ca/openid-connect/keycloak"
-  ]
-  description = "CSS App Created"
-}
