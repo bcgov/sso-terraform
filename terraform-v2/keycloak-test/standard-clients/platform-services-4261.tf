e4b1de85bfd8a9f445d7834a2dc160a04359084e
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-01 13:23:34 -0700

    feat: add a client file for platform-services-4261

---
+++ terraform-v2/keycloak-test/standard-clients/platform-services-4261.tf
@@ -0,0 +1,25 @@
+module "platform-services-4261" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "platform-services-4261"
+  client_name                         = "platform-services"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "githubpublic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://test.loginproxy.gov.bc.ca/auth/realms/platform-services/*"
+  ]
+}
