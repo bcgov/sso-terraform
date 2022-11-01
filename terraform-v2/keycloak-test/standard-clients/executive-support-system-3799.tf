bd8b16c8b25866e6c7d157aacfd71be5a205b824
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-01 15:36:17 -0700

    feat: add a client file for executive-support-system-3799

---
+++ terraform-v2/keycloak-test/standard-clients/executive-support-system-3799.tf
@@ -0,0 +1,26 @@
+module "executive-support-system-3799" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "executive-support-system-3799"
+  client_name                         = "Executive Support System"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://test.cabops.gov.bc.ca/",
+    "https://test.cabops.gov.bc.ca/*",
+    "https://test.cabops.gov.bc.ca/openid-connect/keycloak"
+  ]
+}
