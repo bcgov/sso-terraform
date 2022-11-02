eab3cf6deb7862e230ae45e9bc1001b9e8eb7d4c
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-02 16:25:28 -0700

    feat: add a client file for energy-and-mines-digital-trust-4333

---
+++ terraform-v2/keycloak-test/standard-clients/energy-and-mines-digital-trust-4333.tf
@@ -0,0 +1,24 @@
+module "energy-and-mines-digital-trust-4333" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "energy-and-mines-digital-trust-4333"
+  client_name                         = "Energy and Mines Digital Trust"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://test.loginproxy.gov.bc.ca/auth/realms/digitaltrust-nrm/*"
+  ]
+}
