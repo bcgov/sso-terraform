2d36299aeaecabe13e4db886c31851f0c93ad414
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-03 15:36:34 -0700

    feat: add a client file for itvr-2674

---
+++ terraform/keycloak-test/realms/onestopauth-basic/client-itvr-2674.tf
@@ -0,0 +1,16 @@
+module "client_itvr-2674" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "itvr-2674"
+  client_name = "itvr-2674"
+  valid_redirect_uris = [
+    "https://itvr-test.apps.silver.devops.gov.bc.ca/*"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://itvr-test.apps.silver.devops.gov.bc.ca/*",
+    "+"
+  ]
+}
