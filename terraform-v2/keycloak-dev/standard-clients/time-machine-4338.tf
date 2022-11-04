20a6c44fb784a8d9c4a7e75d41876fdf424894e7
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-04 10:41:44 -0700

    feat: add a client file for time-machine-4338

---
+++ terraform-v2/keycloak-dev/standard-clients/time-machine-4338.tf
@@ -0,0 +1,32 @@
+module "time-machine-4338" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "time-machine-4338"
+  client_name                         = "TimeMachine"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:8080/*",
+    "https://web-ed57f0-dev.apps.silver.devops.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:8080/*",
+    "https://web-ed57f0-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
