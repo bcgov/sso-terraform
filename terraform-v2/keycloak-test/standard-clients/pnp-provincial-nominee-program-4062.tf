73e9874023d0dd1fa27ba342c057d0280833e9c4
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-08 15:15:09 -0800

    feat: add a client file for pnp-provincial-nominee-program-4062

---
+++ terraform-v2/keycloak-test/standard-clients/pnp-provincial-nominee-program-4062.tf
@@ -0,0 +1,28 @@
+module "pnp-provincial-nominee-program-4062" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "pnp-provincial-nominee-program-4062"
+  client_name                         = "PNP - Provincial Nominee Program"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:8000/auth/cb/keycloak/",
+    "https://test-assessment.pnpapplication.gov.bc.ca/auth/oauth-callback",
+    "https://test2-assessment.pnpapplication.gov.bc.ca/auth/oauth-callback",
+    "https://uat-assessment.pnpapplication.gov.bc.ca/auth/oauth-callback",
+    "https://uat2-assessment.pnpapplication.gov.bc.ca/auth/oauth-callback"
+  ]
+}
