1c03d1df47ba21112d869eee4423b29740865dfe
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-09 09:33:04 -0800

    feat: add a client file for community-corrections-case-management-3650

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-community-corrections-case-management-3650.tf
@@ -0,0 +1,10 @@
+module "client_community-corrections-case-management-3650" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "community-corrections-case-management-3650"
+  client_name = "community-corrections-case-management-3650"
+  valid_redirect_uris = [
+    "https://cccm-keycloak-abb712-dev.apps.gold.devops.gov.bc.ca/*"
+  ]
+  description = "CSS App Created"
+}
