68df8738e274b10ddf77ffc284b2a491497bab5a
Junmin Ahn <junminahn@outlook.com>
2022-11-11 08:35:48 -0800

    chore: update terraform custom config

--- terraform-v2-custom/config.tf
+++ terraform-v2-custom/config.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 1.1.4"
 
   backend "s3" {
-    bucket = "xgr00q-prod-keycloak"
-    key    = "keycloak/gold-custom"
-    region = "ca-central-1"
+    bucket         = "xgr00q-prod-keycloak"
+    key            = "keycloak/gold-custom"
+    region         = "ca-central-1"
+    dynamodb_table = "xgr00q-prod-state-locking"
   }
 
   required_providers {
--- terraform/config.tf
+++ terraform/config.tf
@@ -2,8 +2,16 @@ terraform {
   required_version = ">= 0.15.3"
 
   backend "s3" {
-    bucket = "xgr00q-prod-keycloak"
-    key    = "keycloak/silver"
-    region = "ca-central-1"
+    bucket         = "xgr00q-prod-keycloak"
+    key            = "keycloak/silver"
+    dynamodb_table = "xgr00q-prod-state-locking"
+    region         = "ca-central-1"
+  }
+
+  required_providers {
+    keycloak = {
+      source  = "mrparkers/keycloak"
+      version = "3.10.0"
+    }
   }
 }
