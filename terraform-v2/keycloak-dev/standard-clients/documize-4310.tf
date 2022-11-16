57dc025838d3c17542a607a5c574111d39eba830
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-16 10:52:10 -0800

    feat: add a client file for documize-4310

---
+++ terraform-v2/keycloak-dev/standard-clients/documize-4310.tf
@@ -0,0 +1,40 @@
+module "documize-4310" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "documize-4310"
+  client_name                         = "documize"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbusiness",
+    "githubbcgov",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://dds-authz-dev.apps.gov.bc.ca/realms/main/broker/oidc/endpoint",
+    "https://dds-authz-dev.apps.gov.bc.ca/realms/master/broker/oidc/endpoint",
+    "https://dds-authz.apps.gov.bc.ca/realms/main/broker/oidc/endpoint",
+    "https://dev-documize.data.gov.bc.ca/*",
+    "https://documize.data.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://dds-authz-dev.apps.gov.bc.ca/realms/main/broker/oidc/endpoint",
+    "https://dds-authz-dev.apps.gov.bc.ca/realms/master/broker/oidc/endpoint",
+    "https://dds-authz.apps.gov.bc.ca/realms/main/broker/oidc/endpoint",
+    "https://dev-documize.data.gov.bc.ca/*",
+    "https://documize.data.gov.bc.ca/*"
+  ]
+}
