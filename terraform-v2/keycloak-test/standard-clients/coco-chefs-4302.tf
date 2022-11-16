f102961c40da5b963abc9c54d1a73f5df3ef4fc9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-16 13:21:45 -0800

    feat: add a client file for coco-chefs-4302

---
+++ terraform-v2/keycloak-test/standard-clients/coco-chefs-4302.tf
@@ -0,0 +1,26 @@
+module "coco-chefs-4302" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "coco-chefs-4302"
+  client_name                         = "coco-chefs"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://chefs-test.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
