80b443340036fb7c848887021dbd1afc10b5cdc9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-17 10:04:23 -0800

    feat: add a client file for hmcr-4029

---
+++ terraform-v2/keycloak-test/standard-clients/hmcr-4029.tf
@@ -0,0 +1,27 @@
+module "hmcr-4029" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "hmcr-4029"
+  client_name                         = "HMCR"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://test.loginproxy.gov.bc.ca/auth/realms/moti-custom/*",
+    "https://tst-hmcr.th.gov.bc.ca/*"
+  ]
+}
