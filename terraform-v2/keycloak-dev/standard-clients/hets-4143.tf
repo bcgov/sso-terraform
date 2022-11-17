006fe57d79439ccf644d5e2cfd21ac2b71212cee
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-17 13:05:53 -0800

    feat: add a client file for hets-4143

---
+++ terraform-v2/keycloak-dev/standard-clients/hets-4143.tf
@@ -0,0 +1,38 @@
+module "hets-4143" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "hets-4143"
+  client_name                         = "HETS"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbusiness",
+    "githubbcgov",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000/",
+    "http://localhost:3000/*",
+    "https://dev-hets.th.gov.bc.ca/",
+    "https://dev-hets.th.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000/",
+    "http://localhost:3000/*",
+    "https://dev-hets.th.gov.bc.ca/",
+    "https://dev-hets.th.gov.bc.ca/*"
+  ]
+}
