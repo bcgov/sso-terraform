f88a532cc2f0a5c618dc58c41e11d6305c9231b6
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-21 14:03:42 -0800

    feat: add a client file for registries-4402

---
+++ terraform-v2/keycloak-test/standard-clients/registries-4402.tf
@@ -0,0 +1,26 @@
+module "registries-4402" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "registries-4402"
+  client_name                         = "registries"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "githubbcgov",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://dev.loginproxy.gov.bc.ca/auth/realms/bcregistry/*"
+  ]
+}
