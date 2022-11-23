2ec71c47906bbe3bb1c575fb620fb363684d248c
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-23 13:05:21 -0800

    chore: remove a client file for work-bc-job-board-496

--- terraform/keycloak-test/realms/onestopauth/client-work-bc-job-board-496.tf
+++
@@ -1,9 +0,0 @@
-module "client_work-bc-job-board-496" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "work-bc-job-board-496"
-  valid_redirect_uris = [
-    "https://test-admin-jobboard.workbc.ca/*"
-  ]
-  web_origins = ["https://test-admin-jobboard.workbc.ca"]
-}
