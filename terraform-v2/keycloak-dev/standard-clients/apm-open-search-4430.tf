120d69d65a95e1e438e5159ca355c287c18f2d23
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-11-30 09:48:48 -0800

    feat: add a client file for apm-open-search-4430

---
+++ terraform-v2/keycloak-dev/standard-clients/apm-open-search-4430.tf
@@ -0,0 +1,18 @@
+module "apm-open-search-4430" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "apm-open-search-4430"
+  client_name        = "APM - OpenSearch"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://apm.io.nrs.gov.bc.ca/*"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
