852c3dac637850abe5f8171c5e348881e47288f8
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-01 13:10:18 -0800

    feat: add a client file for knox-4161

---
+++ terraform-v2/keycloak-dev/standard-clients/knox-4161.tf
@@ -0,0 +1,26 @@
+module "knox-4161" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "knox-4161"
+  client_name                         = "Knox"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:8250/oidc/callback",
+    "https://vault-iit-dev.apps.silver.devops.gov.bc.ca/ui/*",
+    "https://vault-iit-dev.apps.silver.devops.gov.bc.ca/v1/*"
+  ]
+}
