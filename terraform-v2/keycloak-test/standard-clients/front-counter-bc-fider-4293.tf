cf2d8af328bb04e4b06e882257155c1a04791acf
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-01 08:59:28 -0800

    feat: add a client file for front-counter-bc-fider-4293

---
+++ terraform-v2/keycloak-test/standard-clients/front-counter-bc-fider-4293.tf
@@ -0,0 +1,24 @@
+module "front-counter-bc-fider-4293" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "front-counter-bc-fider-4293"
+  client_name                         = "Front Counter BC Fider"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://fcbctestfider.apps.gold.devops.gov.bc.ca/"
+  ]
+}
