70bc3c9de5dc68e3301bbff0ea271e79e7aaae57
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-01 11:03:39 -0800

    feat: add a client file for wordpress-4439

---
+++ terraform-v2/keycloak-dev/standard-clients/wordpress-4439.tf
@@ -0,0 +1,24 @@
+module "wordpress-4439" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "wordpress-4439"
+  client_name                         = "wordpress"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://des-test.apps.silver.devops.gov.bc.ca"
+  ]
+}
