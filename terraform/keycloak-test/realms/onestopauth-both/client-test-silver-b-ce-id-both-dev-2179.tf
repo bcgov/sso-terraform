1e0ea5b382bd5eac71bc250e16e928ebbe0147e9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-06 09:26:39 -0800

    feat: add a client file for test-silver-b-ce-id-both-dev-2179

---
+++ terraform/keycloak-test/realms/onestopauth-both/client-test-silver-b-ce-id-both-dev-2179.tf
@@ -0,0 +1,16 @@
+module "client_test-silver-b-ce-id-both-dev-2179" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "test-silver-b-ce-id-both-dev-2179"
+  client_name = "test-silver-b-ce-id-both-dev-2179"
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+}
