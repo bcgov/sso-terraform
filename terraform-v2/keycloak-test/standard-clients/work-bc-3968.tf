59b98e06aecce2a5910f731a10666f3b0f8c4fca
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-06 15:52:48 -0800

    feat: add a client file for work-bc-3968

---
+++ terraform-v2/keycloak-test/standard-clients/work-bc-3968.tf
@@ -0,0 +1,24 @@
+module "work-bc-3968" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "work-bc-3968"
+  client_name                         = "WorkBC"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://aws-test.workbc.ca/openid-connect/keycloak"
+  ]
+}
