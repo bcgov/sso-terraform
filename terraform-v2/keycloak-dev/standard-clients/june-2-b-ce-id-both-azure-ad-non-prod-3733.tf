717da3c6ccbe20db28c578f0dd10fd87b5573936
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-06 09:40:59 -0800

    feat: add a client file for june-2-b-ce-id-both-azure-ad-non-prod-3733

---
+++ terraform-v2/keycloak-dev/standard-clients/june-2-b-ce-id-both-azure-ad-non-prod-3733.tf
@@ -0,0 +1,31 @@
+module "june-2-b-ce-id-both-azure-ad-non-prod-3733" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "june-2-b-ce-id-both-azure-ad-non-prod-3733"
+  client_name                         = "June 2 BCeID Both & Azure AD non prod"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
