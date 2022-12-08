0ac0260c7a950c258574bc098ac84f0e3fa8cf6f
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-08 13:56:17 -0800

    feat: add a client file for forest-client-4070

---
+++ terraform-v2/keycloak-test/standard-clients/forest-client-4070.tf
@@ -0,0 +1,31 @@
+module "forest-client-4070" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "forest-client-4070"
+  client_name                         = "Forest Client"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://nr-forest-client-test-frontend.apps.silver.devops.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://nr-forest-client-test-frontend.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
