1acba03318f87e69bfa8c5abda8167a07837a58c
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-13 15:46:12 -0800

    feat: add a client file for bio-hub-browser-4230

---
+++ terraform-v2/keycloak-dev/standard-clients/bio-hub-browser-4230.tf
@@ -0,0 +1,32 @@
+module "bio-hub-browser-4230" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "bio-hub-browser-4230"
+  client_name                         = "BiodiversityHub BC Dev"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "*"
+  ]
+}
