3f8bae9b1bf0374d4048d86ce06400f523221924
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-13 14:57:30 -0800

    feat: add a client file for tidd-trade-industry-development-2575

---
+++ terraform/keycloak-test/realms/onestopauth/client-tidd-trade-industry-development-2575.tf
@@ -0,0 +1,16 @@
+module "client_tidd-trade-industry-development-2575" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=main"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "tidd-trade-industry-development-2575"
+  client_name = "tidd-trade-industry-development-2575"
+  valid_redirect_uris = [
+    "https://tradeandinvestbc--test.my.salesforce.com/"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://tradeandinvestbc--test.my.salesforce.com/",
+    "+"
+  ]
+}
