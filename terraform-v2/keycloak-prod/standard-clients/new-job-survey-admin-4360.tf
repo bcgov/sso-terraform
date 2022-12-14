8b92962affe0fa21bea2129b64f01bac0f9e1960
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-14 11:04:48 -0800

    feat: add a client file for new-job-survey-admin-4360

---
+++ terraform-v2/keycloak-prod/standard-clients/new-job-survey-admin-4360.tf
@@ -0,0 +1,32 @@
+module "new-job-survey-admin-4360" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "new-job-survey-admin-4360"
+  client_name                         = "NewJobSurveyAdmin"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = "user_roles"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000/*",
+    "https://localhost:5051/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://localhost:5051/*"
+  ]
+}
