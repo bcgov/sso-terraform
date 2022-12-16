a37e8d09c1fffb2172c53421c47f5abfab732083
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-16 09:29:51 -0800

    feat: add a client file for ggircs-4112

---
+++ terraform-v2/keycloak-prod/standard-clients/ggircs-4112.tf
@@ -0,0 +1,26 @@
+module "ggircs-4112" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "ggircs-4112"
+  client_name                         = "GGIRCS"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = "groups"
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://cas-ggircs.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
