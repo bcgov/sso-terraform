91598076b670a5381a9c27e4cc34500672465b00
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-19 16:28:32 -0800

    feat: add a client file for ministry-of-finance-intranet-3804

---
+++ terraform-v2/keycloak-dev/standard-clients/ministry-of-finance-intranet-3804.tf
@@ -0,0 +1,33 @@
+module "ministry-of-finance-intranet-3804" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "ministry-of-finance-intranet-3804"
+  client_name                         = "Ministry of Finance Intranet"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:8000/",
+    "http://localhost:8000/*",
+    "http://localhost:8000/openid-connect/keycloak",
+    "https://dev.intranet.fin.gov.bc.ca/",
+    "https://dev.intranet.fin.gov.bc.ca/*",
+    "https://dev.intranet.fin.gov.bc.ca/openid-connect/keycloak",
+    "https://fin-intra-app-route-a1a681-dev.apps.silver.devops.gov.bc.ca/",
+    "https://fin-intra-app-route-a1a681-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://fin-intra-app-route-a1a681-dev.apps.silver.devops.gov.bc.ca/openid-connect/keycloak"
+  ]
+}
