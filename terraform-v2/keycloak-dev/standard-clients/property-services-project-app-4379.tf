a869added1da055462cef7e9256436bc0b2a8a4e
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-22 19:10:49 -0800

    feat: add a client file for property-services-project-app-4379

---
+++ terraform-v2/keycloak-dev/standard-clients/property-services-project-app-4379.tf
@@ -0,0 +1,41 @@
+module "property-services-project-app-4379" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "property-services-project-app-4379"
+  client_name                         = "Property Services Project"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000/*",
+    "https://dev-pims.th.gov.bc.ca/*",
+    "https://pims-app-3cd915-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://pims-app-devin-3cd915-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://pims-app-test-3cd915-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://tst-pims.th.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://dev-pims.th.gov.bc.ca/*",
+    "https://pims-app-3cd915-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://pims-app-devin-3cd915-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://pims-app-test-3cd915-dev.apps.silver.devops.gov.bc.ca/*",
+    "https://tst-pims.th.gov.bc.ca/*"
+  ]
+}
