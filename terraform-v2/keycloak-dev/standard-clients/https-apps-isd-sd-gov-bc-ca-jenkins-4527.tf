26beaf31bf4cfd24bb5fde715031bc64fb10c9a3
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-03 15:30:15 -0800

    feat: add a client file for https-apps-isd-sd-gov-bc-ca-jenkins-4527

---
+++ terraform-v2/keycloak-dev/standard-clients/https-apps-isd-sd-gov-bc-ca-jenkins-4527.tf
@@ -0,0 +1,26 @@
+module "https-apps-isd-sd-gov-bc-ca-jenkins-4527" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "https-apps-isd-sd-gov-bc-ca-jenkins-4527"
+  client_name                         = "https://apps-isd.sd.gov.bc.ca/jenkins"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = "bcgov-idp-stopper-no-header-title"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://apps-isd.sd.gov.bc.ca/jenkins/*"
+  ]
+}
