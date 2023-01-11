5c7667e0f1dba0c487a89dd015f3cd3067bf405a
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-11 10:28:01 -0800

    feat: add a client file for tians-testing-11-1-service-account-4542

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-testing-11-1-service-account-4542.tf
@@ -0,0 +1,21 @@
+module "tians-testing-11-1-service-account-4542" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-11-1-service-account-4542"
+  client_name                         = "Tian's Testing - 11/1 Service Account"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = false
+  service_accounts_enabled     = true
+}
