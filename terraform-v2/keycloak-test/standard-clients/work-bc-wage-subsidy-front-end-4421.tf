c9b71812a4881aff4c5cafe053cfe284be7b5449
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-11 10:19:08 -0800

    feat: add a client file for work-bc-wage-subsidy-front-end-4421

---
+++ terraform-v2/keycloak-test/standard-clients/work-bc-wage-subsidy-front-end-4421.tf
@@ -0,0 +1,32 @@
+module "work-bc-wage-subsidy-front-end-4421" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "work-bc-wage-subsidy-front-end-4421"
+  client_name                         = "WorkBC Wage Subsidy Front-end"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "*"
+  ]
+}
