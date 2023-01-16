0e5919ca3062081faf3a7f5f2d02b860133596f5
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-16 08:12:17 -0800

    feat: add a client file for bc-telemetry-warehouse-3766

---
+++ terraform-v2/keycloak-dev/standard-clients/bc-telemetry-warehouse-3766.tf
@@ -0,0 +1,30 @@
+module "bc-telemetry-warehouse-3766" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "bc-telemetry-warehouse-3766"
+  client_name                         = "BC Telemetry Warehouse (DEV)"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost/*",
+    "http://localhost:1111/*",
+    "http://localhost:80/*",
+    "http://localhost:8080/*",
+    "http://moe-bctw-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
