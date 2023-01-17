d61cb78a1936b57109a38e1b4e5ec1d0784f6fc9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-17 10:12:15 -0800

    feat: add a client file for on-route-bc-4452

---
+++ terraform-v2/keycloak-test/standard-clients/on-route-bc-4452.tf
@@ -0,0 +1,28 @@
+module "on-route-bc-4452" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "on-route-bc-4452"
+  client_name                         = "OnRouteBC"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://localhost:3000/oauth/callback",
+    "https://tst.onroutebc.gov.bc.ca/*"
+  ]
+}
