8846caf4f0f7c6e619e4436324ac99bd6761f2d5
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-19 13:18:25 -0800

    feat: add a client file for community-corrections-case-management-3997

---
+++ terraform-v2/keycloak-dev/standard-clients/community-corrections-case-management-3997.tf
@@ -0,0 +1,25 @@
+module "community-corrections-case-management-3997" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "community-corrections-case-management-3997"
+  client_name                         = "Community Corrections Case Management"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://cccm-keycloak-abb712-dev.apps.gold.devops.gov.bc.ca/*"
+  ]
+}
