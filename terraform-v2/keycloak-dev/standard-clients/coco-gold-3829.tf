a9d26ab008b5467181d4aa37484ae9e088956087
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-01-31 08:39:54 -0800

    feat: add a client file for coco-gold-3829

---
+++ terraform-v2/keycloak-dev/standard-clients/coco-gold-3829.tf
@@ -0,0 +1,31 @@
+module "coco-gold-3829" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "coco-gold-3829"
+  client_name                         = "CHEFS Login"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://192.168.1.65:8082/*",
+    "http://localhost:8080/*",
+    "http://localhost:8081/*",
+    "http://localhost:8082/*",
+    "https://localhost:8080/*"
+  ]
+}
