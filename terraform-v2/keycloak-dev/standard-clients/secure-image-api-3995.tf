19b9939c33443f2423ee7982ebffdfa11e95e2bc
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-01 10:58:19 -0800

    feat: add a client file for secure-image-api-3995

---
+++ terraform-v2/keycloak-dev/standard-clients/secure-image-api-3995.tf
@@ -0,0 +1,25 @@
+module "secure-image-api-3995" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "secure-image-api-3995"
+  client_name                         = "Secure Image"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://dev-secure-image-kcg.apps.silver.devops.gov.bc.ca/v1/auth/callback"
+  ]
+}
