57ea2effba359ca7d1ddfe6be0f1bee8be73bda9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-03 13:23:44 -0800

    feat: add a client file for tians-testing-2-3-keycloak-upgrade-testing-2-4574

---
+++ terraform-v2/keycloak-test/standard-clients/tians-testing-2-3-keycloak-upgrade-testing-2-4574.tf
@@ -0,0 +1,33 @@
+module "tians-testing-2-3-keycloak-upgrade-testing-2-4574" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-2-3-keycloak-upgrade-testing-2-4574"
+  client_name                         = "Tian's Testing 2/3 Keycloak Upgrade testing 2"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "githubbcgov",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
