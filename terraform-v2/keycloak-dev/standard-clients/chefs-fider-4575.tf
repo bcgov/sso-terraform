f0827dd97c6385a00a9c31fb16816611efbabb9c
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-05 22:08:43 -0800

    feat: add a client file for chefs-fider-4575

---
+++ terraform-v2/keycloak-dev/standard-clients/chefs-fider-4575.tf
@@ -0,0 +1,25 @@
+module "chefs-fider-4575" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "chefs-fider-4575"
+  client_name                         = "CHEFS Fider"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://chefs-fider.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
