f3dc8439490bf6fba2dff25b77f0c37710bfd92c
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-06 11:25:12 -0800

    feat: add a client file for pbgp-4412

---
+++ terraform-v2/keycloak-dev/standard-clients/pbgp-4412.tf
@@ -0,0 +1,33 @@
+module "pbgp-4412" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "pbgp-4412"
+  client_name                         = "PBGP"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000/*",
+    "https://pbgp-dev.apps.silver.devops.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://pbgp-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
