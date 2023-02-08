e587cbf9fb6095bae1533c97d6cd401d16feb306
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-08 10:30:17 -0800

    feat: add a client file for bceid-registries-idp-4349

---
+++ terraform-v2/keycloak-dev/standard-clients/bceid-registries-idp-4349.tf
@@ -0,0 +1,25 @@
+module "bceid-registries-idp-4349" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "bceid-registries-idp-4349"
+  client_name                         = "bceid-registries-idp"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://dev.loginproxy.gov.bc.ca/auth/realms/bcregistry/*"
+  ]
+}
