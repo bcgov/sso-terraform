cf0770c85ff3093c34aa9314df09fc6cd6f413a2
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-08 09:39:17 -0800

    feat: add a client file for e-tk-sso-4533

---
+++ terraform-v2/keycloak-prod/standard-clients/e-tk-sso-4533.tf
@@ -0,0 +1,35 @@
+module "e-tk-sso-4533" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "e-tk-sso-4533"
+  client_name                         = "eTk SSO"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://jh-etk-scweb.pathfinder.bcgov/*",
+    "https://jh-etk-scweb*",
+    "https://jh-etk-scweb.apps.silver.devops.gov.bc.ca/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://jh-etk-scweb.pathfinder.bcgov/*",
+    "https://jh-etk-scweb*",
+    "https://jh-etk-scweb.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
