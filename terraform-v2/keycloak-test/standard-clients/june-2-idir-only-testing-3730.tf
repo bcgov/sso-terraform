a759675b8ce9b6b76fdd83f404ac44a68444c2e0
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-13 10:23:56 -0800

    feat: add a client file for june-2-idir-only-testing-3730

---
+++ terraform-v2/keycloak-test/standard-clients/june-2-idir-only-testing-3730.tf
@@ -0,0 +1,27 @@
+module "june-2-idir-only-testing-3730" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "june-2-idir-only-testing-3730"
+  client_name                         = "June 2 IDIR Only Testing"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost/*",
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
