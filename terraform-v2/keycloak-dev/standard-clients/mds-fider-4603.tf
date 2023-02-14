2e2126e15c18be3ef0a98aac9fda6c76903bdbd4
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-14 14:36:23 -0800

    feat: add a client file for mds-fider-4603

---
+++ terraform-v2/keycloak-dev/standard-clients/mds-fider-4603.tf
@@ -0,0 +1,25 @@
+module "mds-fider-4603" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "mds-fider-4603"
+  client_name                         = "MDS-FIDER"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://fider-4c2ba9-test.apps.silver.devops.gov.bc.ca/"
+  ]
+}
