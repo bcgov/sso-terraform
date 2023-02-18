b86fe650fcebef58e8196f87ddfc30fdddc43981
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-17 23:19:07 -0800

    feat: add a client file for jpidp-4624

---
+++ terraform-v2/keycloak-dev/standard-clients/jpidp-4624.tf
@@ -0,0 +1,26 @@
+module "jpidp-4624" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "jpidp-4624"
+  client_name                         = "JPIDP"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://dev.common-sso.justice.gov.bc.ca/auth/realms/IDIR/broker/idir/endpoint",
+    "https://dev.common-sso.justice.gov.bc.ca/auth/realms/IDIR/broker/idirtest/endpoint"
+  ]
+}
