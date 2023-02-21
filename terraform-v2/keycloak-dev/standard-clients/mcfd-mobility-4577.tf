62c49fbf4ca768090335a41fd4251a7791e9718d
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-21 10:33:24 -0800

    feat: add a client file for mcfd-mobility-4577

---
+++ terraform-v2/keycloak-dev/standard-clients/mcfd-mobility-4577.tf
@@ -0,0 +1,34 @@
+module "mcfd-mobility-4577" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "mcfd-mobility-4577"
+  client_name                         = "MCFD Mobility"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "hestia://client",
+    "https://hestia-dev.api.gov.bc.ca",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "hestia://client",
+    "https://hestia-dev.api.gov.bc.ca"
+  ]
+}
