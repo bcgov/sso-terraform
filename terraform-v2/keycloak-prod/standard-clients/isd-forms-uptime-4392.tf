7bf906dcf3da4811284c2ee1135a3a9d3dd9f2ff
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-02-24 12:59:44 -0800

    feat: add a client file for isd-forms-uptime-4392

---
+++ terraform-v2/keycloak-prod/standard-clients/isd-forms-uptime-4392.tf
@@ -0,0 +1,28 @@
+module "isd-forms-uptime-4392" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "isd-forms-uptime-4392"
+  client_name                         = "ISD Uptime"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = "bcgov-idp-stopper-no-header-title"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://obstatus.apps-isd.sd.gov.bc.ca/oauth2/callback",
+    "https://obstatus.apps.gov.bc.ca/*",
+    "https://uptime.api.gov.bc.ca/*",
+    "https://uptime.apps.gov.bc.ca/*"
+  ]
+}
