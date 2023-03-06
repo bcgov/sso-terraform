b78ab42ec0acfcefdd19ff5c1b2874deaf0d3f27
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-06 15:36:56 -0800

    feat: add a client file for apm-open-search-oidc-4567

---
+++ terraform-v2/keycloak-dev/standard-clients/apm-open-search-oidc-4567.tf
@@ -0,0 +1,28 @@
+module "apm-open-search-oidc-4567" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "apm-open-search-oidc-4567"
+  client_name                         = "APM - OpenSearch - OIDC"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://dev-opensearch-user-domain.auth.ca-central-1.amazoncognito.com/*",
+    "https://oidcdebuggersecure-3d5c3f-dev.apps.silver.devops.gov.bc.ca/",
+    "https://search-dev-opensearch-xnpjj3d3kvmo2hzyvpkpoqfgnq.ca-central-1.es.amazonaws.com/_dashboards",
+    "https://search-dev-opensearch-xnpjj3d3kvmo2hzyvpkpoqfgnq.ca-central-1.es.amazonaws.com/_dashboards/*"
+  ]
+}
