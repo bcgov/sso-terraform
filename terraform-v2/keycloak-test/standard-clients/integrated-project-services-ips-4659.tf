f1f079fe4c04de2e0a13226987da5de441ffdd2a
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-09 14:29:06 -0800

    feat: add a client file for integrated-project-services-ips-4659

---
+++ terraform-v2/keycloak-test/standard-clients/integrated-project-services-ips-4659.tf
@@ -0,0 +1,26 @@
+module "integrated-project-services-ips-4659" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "integrated-project-services-ips-4659"
+  client_name                         = "Integrated Project Services (IPS)"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://ipsmotit.pmweb.com/PMWebSAML/"
+  ]
+}
