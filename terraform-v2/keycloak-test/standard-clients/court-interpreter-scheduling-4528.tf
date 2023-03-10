c0ab1bfe9c2860d899120129af7e6002190c42fc
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-10 08:32:45 -0800

    feat: add a client file for court-interpreter-scheduling-4528

---
+++ terraform-v2/keycloak-test/standard-clients/court-interpreter-scheduling-4528.tf
@@ -0,0 +1,27 @@
+module "court-interpreter-scheduling-4528" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "court-interpreter-scheduling-4528"
+  client_name                         = "Court Interpreter Scheduling"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:8080/*",
+    "https://court-interpreter-scheduling-test.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
