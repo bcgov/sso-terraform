1024790b107450011c9bf9313d926a0393a13b41
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-15 18:47:45 -0700

    feat: add a client file for critterbase-4629

---
+++ terraform-v2/keycloak-dev/standard-clients/critterbase-4629.tf
@@ -0,0 +1,29 @@
+module "critterbase-4629" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "critterbase-4629"
+  client_name                         = "Critterbase (DEV)"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://localhost/*",
+    "http://localhost:1111/*",
+    "http://localhost:80/*",
+    "http://localhost:8080/*",
+    "http://moe-critterbase-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
