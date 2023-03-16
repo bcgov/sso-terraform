f3bd20e676e2857e1421b98c84649f8b99d19bbd
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-15 18:42:25 -0700

    chore: remove a client file for service-account-team-827-4163

--- terraform-v2/keycloak-prod/standard-service-accounts/service-account-team-827-4163.tf
+++
@@ -1,7 +0,0 @@
-module "service-account-team-827-4163" {
-  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-service-account"
-  realm_id    = var.standard_realm_id
-  client_id   = "service-account-team-827-4163"
-  team_id     = 827
-  description = "CSS App Created"
-}
