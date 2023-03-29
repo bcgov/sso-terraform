360c939eab94f47a96ae7b20ba2957a4a525aed4
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-29 13:47:32 -0700

    feat: add a client file for august-9-dev-b-ce-id-both-3982

---
+++ terraform-v2/keycloak-dev/standard-clients/august-9-dev-b-ce-id-both-3982.tf
@@ -0,0 +1,34 @@
+module "august-9-dev-b-ce-id-both-3982" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "august-9-dev-b-ce-id-both-3982"
+  client_name                         = "August 9 - Dev BCeID Both"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000/*",
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
