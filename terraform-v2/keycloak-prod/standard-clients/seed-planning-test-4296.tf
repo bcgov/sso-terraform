34e09fd852ecdc7f80817213ac4fc871a3adfa5f
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-04-21 06:00:46 -0700

    feat: add a client file for seed-planning-test-4296

---
+++ terraform-v2/keycloak-prod/standard-clients/seed-planning-test-4296.tf
@@ -0,0 +1,37 @@
+module "seed-planning-test-4296" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "seed-planning-test-4296"
+  client_name                         = "New SPAR"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/dashboard",
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/logout",
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/silent-check-sso",
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/silent-check-sso.html",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/dashboard",
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/logout",
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/silent-check-sso",
+    "https://nr-spar-webapp-prod-frontend.apps.silver.devops.gov.bc.ca/silent-check-sso.html"
+  ]
+}
