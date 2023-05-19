fadb632fb7a3d7a90bac34f578d78b9fd485f7dc
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-05-19 08:43:05 -0700

    feat: add a client file for css-app-in-gold-4128

---
+++ terraform-v2/keycloak-prod/standard-clients/css-app-in-gold-4128.tf
@@ -0,0 +1,34 @@
+module "css-app-in-gold-4128" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "css-app-in-gold-4128"
+  client_name                         = "Common Hosted Single Sign-on"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/sso-requests-preview/*",
+    "https://bcgov.github.io/sso-requests/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/sso-requests-preview/*",
+    "https://bcgov.github.io/sso-requests/*"
+  ]
+}
