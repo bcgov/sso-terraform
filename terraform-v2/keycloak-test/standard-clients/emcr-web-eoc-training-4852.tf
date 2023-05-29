ad4a1c773ad22281122727b4abd84b8643c4e89a
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-05-29 15:06:49 -0700

    feat: add a client file for emcr-web-eoc-training-4852

---
+++ terraform-v2/keycloak-test/standard-clients/emcr-web-eoc-training-4852.tf
@@ -0,0 +1,19 @@
+module "emcr-web-eoc-training-4852" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "emcr-web-eoc-training-4852"
+  client_name        = "EMCR WebEOC TRAINING"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://bctraining.demo.webeocasp.com/bctraining/*"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
