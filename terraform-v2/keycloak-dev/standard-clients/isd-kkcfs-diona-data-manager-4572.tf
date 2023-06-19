c599ef4a30aa70cc9448959095a5f778687f7eee
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-06-19 12:00:04 -0700

    feat: add a client file for isd-kkcfs-diona-data-manager-4572

---
+++ terraform-v2/keycloak-dev/standard-clients/isd-kkcfs-diona-data-manager-4572.tf
@@ -0,0 +1,25 @@
+module "isd-kkcfs-diona-data-manager-4572" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "isd-kkcfs-diona-data-manager-4572"
+  client_name                         = "ISD-KKCFS-DIONA-DataManager"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://kkcfs.diona.mobi:9011/oauth2/redirect"
+  ]
+}
