1fbfd7177e9bb89ee6d11f369a8b5e962ff66d1c
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-07-26 14:40:27 -0700

    feat: add a client file for bc-stats-co-ms-4938

---
+++ terraform-v2/keycloak-test/standard-clients/bc-stats-co-ms-4938.tf
@@ -0,0 +1,26 @@
+module "bc-stats-co-ms-4938" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "bc-stats-co-ms-4938"
+  client_name                         = "BCStats COMs"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost*"
+  ]
+}
