b111ad275da49949790d9fbe1f01b5b22e6da154
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-07-31 09:07:10 -0700

    feat: add a client file for met-gdx-4948

---
+++ terraform-v2/keycloak-test/standard-clients/met-gdx-4948.tf
@@ -0,0 +1,25 @@
+module "met-gdx-4948" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "met-gdx-4948"
+  client_name                         = "MET-GDX"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://met-oidc-test.apps.gold.devops.gov.bc.ca/auth/realms/met/broker/idir/endpoint/*"
+  ]
+}
