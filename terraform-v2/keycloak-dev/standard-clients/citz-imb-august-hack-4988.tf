9f32a15197411977ecac802950922e1da5c57e34
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-08-17 14:11:41 -0700

    feat: add a client file for citz-imb-august-hack-4988

---
+++ terraform-v2/keycloak-dev/standard-clients/citz-imb-august-hack-4988.tf
@@ -0,0 +1,26 @@
+module "citz-imb-august-hack-4988" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "citz-imb-august-hack-4988"
+  client_name                         = "citz-imb-august-hack"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://hollandaise-frontend-ec1236-test.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
