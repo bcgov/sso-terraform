c85e2c51d1f26068f4621d7b89bcef946c429d5b
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-09-26 16:57:44 -0700

    feat: add a client file for gdx-browserstack-5012

---
+++ terraform-v2/keycloak-dev/standard-clients/gdx-browserstack-5012.tf
@@ -0,0 +1,24 @@
+module "gdx-browserstack-5012" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "gdx-browserstack-5012"
+  client_name        = "GDX Browserstack - DEV"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://live.browserstack.com/*",
+    "https://login.browserstack.com/auth/realms/6d0c9eb9d7c86a9e0258b275b467c4f3",
+    "https://login.browserstack.com/auth/realms/6d0c9eb9d7c86a9e0258b275b467c4f3/broker/f5f6d13d51a9bb64426cbcf69b380c17/endpoint",
+    "https://login.browserstack.com/auth/realms/6d0c9eb9d7c86a9e0258b275b467c4f3/broker/f5f6d13d51a9bb64426cbcf69b380c17/endpoint/clients/f5f6d13d51a9bb64426cbcf69b380c17",
+    "https://test.alpha.qa.gov.bc.ca/*",
+    "https://www.browserstack.com/*"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = "https://www.browserstack.com/"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
