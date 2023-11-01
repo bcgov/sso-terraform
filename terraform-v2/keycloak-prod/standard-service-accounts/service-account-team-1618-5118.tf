e94d56722ec79c5226f068cd1168aa4aaaee40aa
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-11-01 09:55:25 -0700

    feat: add a client file for service-account-for-team-1618-5118

---
+++ terraform-v2/keycloak-prod/standard-service-accounts/service-account-team-1618-5118.tf
@@ -0,0 +1,7 @@
+module "service-account-team-1618-5118" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-service-account"
+  realm_id    = var.standard_realm_id
+  client_id   = "service-account-team-1618-5118"
+  team_id     = 1618
+  description = "CSS App Created"
+}
