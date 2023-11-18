1ec6b38dba0f3aae6333cf745de3f6c1809d37f9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-11-17 16:02:47 -0800

    feat: add a client file for obps-4953

---
+++ terraform-v2/keycloak-dev/standard-clients/obps-4953.tf
@@ -0,0 +1,29 @@
+module "obps-4953" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "obps-4953"
+  client_name                         = "OBPS"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "http://localhost:8000/*",
+    "https://cas-reg-frontend-dev.apps.silver.devops.gov.bc.ca/*"
+  ]
+}
