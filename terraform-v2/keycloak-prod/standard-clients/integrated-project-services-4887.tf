f9ac6e13780cfc6caa903aa9d8dcfaa007e36965
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-11-20 10:18:14 -0800

    feat: add a client file for integrated-project-services-4887

---
+++ terraform-v2/keycloak-prod/standard-clients/integrated-project-services-4887.tf
@@ -0,0 +1,20 @@
+module "integrated-project-services-4887" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "integrated-project-services-4887"
+  client_name        = "Integrated Project Services"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://ipsmoti.pmweb.com/PMWebSAML/"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = ""
+  sign_assertions              = false
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
