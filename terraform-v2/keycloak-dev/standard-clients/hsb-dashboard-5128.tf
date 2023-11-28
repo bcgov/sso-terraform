d956d4bc6b99cc39bea74fdc00a7d48c23029a53
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-11-28 11:43:15 -0800

    feat: add a client file for hsb-dashboard-5128

---
+++ terraform-v2/keycloak-dev/standard-clients/hsb-dashboard-5128.tf
@@ -0,0 +1,28 @@
+module "hsb-dashboard-5128" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "hsb-dashboard-5128"
+  client_name                         = "HSB Consumption Management Dashboard"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://dev-hsb.apps.emerald.devops.gov.bc.ca/",
+    "https://dev-hsb.apps.emerald.devops.gov.bc.ca/*",
+    "https://dev-hsb.apps.emerald.devops.gov.bc.ca/api/auth/callback/keycloak"
+  ]
+}
