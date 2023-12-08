0212ae490e1d0117d6825da80f7f6591c92943d5
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-12-08 15:16:46 -0800

    feat: add a client file for bc-parks-dam-4602

---
+++ terraform-v2/keycloak-dev/standard-clients/bc-parks-dam-4602.tf
@@ -0,0 +1,20 @@
+module "bc-parks-dam-4602" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=main/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "bc-parks-dam-4602"
+  client_name        = "BC Parks DAM"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://dam.lqc63d-dev.nimbus.cloud.gov.bc.ca/plugins/simplesaml/lib/www/module.php/saml/sp/saml2-acs.php/resourcespace-sp"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = ""
+  sign_assertions              = false
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
