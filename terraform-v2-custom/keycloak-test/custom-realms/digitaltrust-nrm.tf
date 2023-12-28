bf592ba631d3d47cea8b11a2a0b840c5c985356b
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-12-28 09:08:12 -0800

    feat: disable custom realm digitaltrust-nrm

--- terraform-v2-custom/keycloak-test/custom-realms/digitaltrust-nrm.tf
+++ terraform-v2-custom/keycloak-test/custom-realms/digitaltrust-nrm.tf
@@ -1,5 +1,5 @@
 module "digitaltrust-nrm" {
   source     = "github.com/bcgov/sso-terraform-modules?ref=main/modules/custom-realm"
   realm_name = "digitaltrust-nrm"
-  enabled    = true
+  enabled    = false
 }
