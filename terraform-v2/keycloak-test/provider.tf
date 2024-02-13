a859e7bc6cdc7dc962e41e36a7ac55952f4d33aa
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2024-02-13 10:27:29 -0800

    feat: upgrade tf kc provider to 4.4.0 (#3168)

--- terraform-v2/keycloak-test/provider.tf
+++ terraform-v2/keycloak-test/provider.tf
@@ -2,5 +2,5 @@ provider "keycloak" {
   client_id = var.client_id
   username  = var.username
   password  = var.password
-  url       = var.keycloak_url
+  url       = "${var.keycloak_url}/auth"
 }
