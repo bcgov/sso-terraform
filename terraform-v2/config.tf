32a290fc99df6dcbd468389af22b58b6d3901a16
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-02-24 12:58:15 -0800

    feat: parse ppid client creds  from action secrets

--- terraform-v2/config.tf
+++ terraform-v2/config.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.1.4"
 
+  backend "s3" {
+    bucket         = "xgr00q-dev-keycloak"
+    key            = "keycloak/gold"
+    dynamodb_table = "xgr00q-dev-state-locking"
+    region         = "ca-central-1"
+  }
+
   required_providers {
     keycloak = {
       source  = "keycloak/keycloak"
