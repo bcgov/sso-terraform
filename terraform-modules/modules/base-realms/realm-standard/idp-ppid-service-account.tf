cbe9d7bd10890ace9e163eb69872f5cc9948ce73
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-02-26 15:16:34 -0800

    fix: setup terraform to include ppid client creds

--- terraform-modules/modules/base-realms/realm-standard/idp-ppid-service-account.tf
+++ terraform-modules/modules/base-realms/realm-standard/idp-ppid-service-account.tf
@@ -1,6 +1,7 @@
 resource "keycloak_oidc_identity_provider" "ppid_service_account" {
   realm             = module.realm.id
   alias             = "ppid-service-account"
+  display_name      = "PPID Service Account"
   authorization_url = var.ppid_api_url
   client_id         = var.ppid_client_id
   client_secret     = var.ppid_client_secret
