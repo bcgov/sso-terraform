a8d5fbb50b0092ad477409db5323da7410b0e5f2
Jonathan Langlois <37274633+jlangy@users.noreply.github.com>
2026-06-23 12:41:36 -0700

    feat: configurable otp provider (#3262)
    make otp provider id and forward params into variables

--- terraform-modules/modules/base-realms/realm-standard/variables.tf
+++ terraform-modules/modules/base-realms/realm-standard/variables.tf
@@ -133,3 +133,15 @@ variable "optional_client_scopes" {
   type        = list(string)
   default     = ["offline_access", "phone", "microprofile-jwt"]
 }
+
+variable "otp_provider_id" {
+  description = "The Provider ID of the Keycloak OIDC IDP SPI to use"
+  type        = string
+  default     = "oidc"
+}
+
+variable "otp_forward_parameters" {
+  description = "Query Parameters to forward to the IDP"
+  type        = string
+  default     = ""
+}
