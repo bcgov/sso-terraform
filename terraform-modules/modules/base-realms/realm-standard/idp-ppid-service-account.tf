f66c2e2c0351eaa4ef12bd4a1a0243bb52ac9207
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-07-22 15:55:10 -0700

    fix: set dummy vars for auth and token urls

--- terraform-modules/modules/base-realms/realm-standard/idp-ppid-service-account.tf
+++ terraform-modules/modules/base-realms/realm-standard/idp-ppid-service-account.tf
@@ -1,9 +1,11 @@
 resource "keycloak_oidc_identity_provider" "ppid_service_account" {
-  realm         = module.realm.id
-  alias         = "ppid-service-account"
-  display_name  = "PPID Service Account"
-  client_id     = var.ppid_client_id
-  client_secret = var.ppid_client_secret
+  realm             = module.realm.id
+  alias             = "ppid-service-account"
+  display_name      = "PPID Service Account"
+  authorization_url = "https://dummy-url/auth"
+  client_id         = var.ppid_client_id
+  client_secret     = var.ppid_client_secret
+  token_url         = "https://dummy-url/token"
 
   extra_config = {
     "clientAuthMethod" = "client_secret_post"
